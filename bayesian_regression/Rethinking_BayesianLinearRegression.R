00feb6294638f46802498084082bcef1be4b9399
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-08-20 00:47:17 +0200

    Update Rethinking_BayesianLinearRegression.R
    studying the precis function and learning how to define a prior.

--- bayesian_regression/Rethinking_BayesianLinearRegression.R
+++ bayesian_regression/Rethinking_BayesianLinearRegression.R
@@ -6,31 +6,16 @@ https://github.com/rmcelreath/rethinking
 
 "
 
-install.packages(c("mvtnorm","loo","coda"), repos="https://cloud.r-project.org/",dependencies=TRUE)
-options(repos=c(getOption('repos'), rethinking='http://xcelab.net/R'))
-install.packages('rethinking',type='source')
+#Example using precis function
+library(rethinking)
+data("Howell1")
+d <- Howell1
+precis(d,prob=0.95)
+summary(result)
+plot(result)
 
-#Installing stan
-#https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started
-remove.packages("rstan")
-if (file.exists(".RData")) file.remove(".RData")
-
-install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
-
-pkgbuild::has_build_tools(debug = TRUE)
-
-#To Make Stan run faster
-dotR <- file.path(Sys.getenv("HOME"), ".R")
-if (!file.exists(dotR)) dir.create(dotR)
-M <- file.path(dotR, ifelse(.Platform$OS.type == "windows", "Makevars.win", "Makevars"))
-if (!file.exists(M)) file.create(M)
-cat("\nCXX14FLAGS=-O3 -march=native -mtune=native",
-    if( grepl("^darwin", R.version$os)) "CXX14FLAGS += -arch x86_64 -ftemplate-depth-256" else 
-      if (.Platform$OS.type == "windows") "CXX11FLAGS=-O3 -march=native -mtune=native" else
-        "CXX14FLAGS += -fPIC",
-    file = M, sep = "\n", append = TRUE)
-
-library("rstan") # observe startup messages
-options(mc.cores = parallel::detectCores())
-rstan_options(auto_write = TRUE)
-Sys.setenv(LOCAL_CPPFLAGS = '-march=native')
+#Build a prior
+sample_mu <- rnorm(1e4, 178, 20)
+sample_sigma <- runif(1e4, 0, 50)
+prior_h <- rnorm(1e4, sample_mu, sample_sigma)
+dens(prior_h)
