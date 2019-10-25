64e0512675baa6f6037094e28d7ce44ba7774872
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-10-25 21:19:48 +0200

    Explanation for WAIC table
    comments

--- model_selection/WAIC_computation.R
+++ model_selection/WAIC_computation.R
@@ -91,6 +91,8 @@ compare(m,m1.1)
 # m    423.0   4.6   0.0   0.58  17.70   NA
 # m1.1 423.7   6.1   0.7   0.42  18.94 4.35
 
+#Small WAIC values are better (models are sorted by WAIC)
+
 compare(m,m1.1, func=LOO)
 #      LOO pLOO dLOO weight   SE   dSE
 # m    4.6  0.5  0.0    0.7  1.76   NA
