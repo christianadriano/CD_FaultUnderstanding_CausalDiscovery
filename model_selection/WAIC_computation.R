cfd51cdf4005985286794b5508c6ea6d4dd36ab0
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-10-25 20:48:31 +0200

    Update WAIC_computation.R
    compared results with WAIC function from rethinking
    also set to compute with multiple cores

--- model_selection/WAIC_computation.R
+++ model_selection/WAIC_computation.R
@@ -12,6 +12,9 @@ devtools::install_github("rmcelreath/rethinking")
 library(rethinking)
 #-----------------------------
 
+#For execution on a local, multicore CPU with excess RAM
+options(mc.cores = parallel::detectCores())
+
 data(cars)
 
 "1. Uses the MAP algorithm to compute the posterior"
@@ -40,16 +43,32 @@ logprob <- sapply( 1:n_samples ,
 "4. compute the lppd"
 n_cases <- nrow(cars)
 lppd <- sapply( 1:n_cases , function(i) log_sum_exp(logprob[i,]) - log(n_samples) )
+sum(lppd)
+#[1] -206.9311
 
 "5. compute penalties for each observation"
 pWAIC <- sapply( 1:n_cases , function(i) var(logprob[i,]) )
 
 "6. compute WAIC"
 -2*( sum(lppd) - sum(pWAIC) )
+#[1] 422.2855
 
 "7. compute WAIC by observation"
 waic_vec <- -2*( lppd - pWAIC )
 
 "8. compute the standard error"
 sqrt( n_cases*var(waic_vec) )
+#[1] 17.35175
+
+"9. Compare with WAIC function results"
+WAIC(m)
+# Constructing posterior predictions
+# [ 1000 / 1000 ]
+# [1] 423.0785
+# attr(,"lppd")
+# [1] -206.9319
+# attr(,"pWAIC")
+# [1] 4.607323
+# attr(,"se")
+# [1] 17.61842
   
