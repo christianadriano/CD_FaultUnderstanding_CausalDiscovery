1abf08c76ba5c87206814a71791cbd81e8673634
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-10-26 16:17:18 +0200

    Update WAIC_computation.R
    compared models

--- model_selection/WAIC_computation.R
+++ model_selection/WAIC_computation.R
@@ -9,8 +9,9 @@ if (file.exists(".RData")) file.remove(".RData")
 install.packages(c("coda","mvtnorm","devtools","loo"))
 library(devtools)
 devtools::install_github("rmcelreath/rethinking")
-library(rethinking)
 #-----------------------------
+
+library(rethinking)
 
 #For execution on a local, multicore CPU with excess RAM
 options(mc.cores = parallel::detectCores())
@@ -52,14 +53,14 @@ pWAIC <- sapply( 1:n_cases , function(i) var(logprob[i,]) )
 
 "6. compute WAIC"
 -2*( sum(lppd) - sum(pWAIC) )
-#[1] 422.2855
+#[1] 423.3336
 
 "7. compute WAIC by observation"
 waic_vec <- -2*( lppd - pWAIC )
 
 "8. compute the standard error"
 sqrt( n_cases*var(waic_vec) )
-#[1] 17.35175
+#[1] 17.83451
 
 "9. Compare with WAIC function results"
 WAIC(m)
@@ -88,12 +89,32 @@ m1.1 <- map(
 
 compare(m,m1.1)
 #       WAIC  pWAIC dWAIC weight    SE  dSE
-# m    423.0   4.6   0.0   0.58  17.70   NA
-# m1.1 423.7   6.1   0.7   0.42  18.94 4.35
+# m    423.0   4.5   0.0   0.55  17.28   NA
+# m1.1 423.7   5.9   0.4   0.45  18.69 4.35
 
 #Small WAIC values are better (models are sorted by WAIC)
 
+#Same comparision can be made using 
+#Pareto-Smoothed Importance Sampling Cross-validaiton
 compare(m,m1.1, func=LOO)
 #      LOO pLOO dLOO weight   SE   dSE
-# m    4.6  0.5  0.0    0.7  1.76   NA
-# m1.1 6.4  0.7  1.7    0.3  2.32 0.72
+# m    4.6  0.5  0.0    0.68  1.86   NA
+# m1.1 6.4  0.6  1.5    0.32  2.29 0.71
+
+"Going back to WAIC table, How much better is m compare to m1.1 ?
+To answer this question, we need to look at the differences 
+between the error. Take the difference in standard error (dse in 
+m1.1 line). Let us first see how was this computed:"
+dwaic_m <- WAIC(m, pointwise = TRUE)
+dwaic_m1.1 <- WAIC(m1.1, pointwise = TRUE)
+diff_m1.1_m <- dwaic_m - dwaic_m1.1
+
+"Is this value large? We can check if this value causes 
+the dWAIC to cross the zero. To do that, we can calculate the
+a 99% interval difference, which has the corresponding z-score 2.6."
+
+#interval_differnce:
+dwaic <- 0.7 #from compare WAIC table
+dse_m1.1 <- 4.35
+z_score <- 2.6 #corresponding to 99% interval difference
+dwaic + c(-1,1) * dse_m1.1 * z_score
