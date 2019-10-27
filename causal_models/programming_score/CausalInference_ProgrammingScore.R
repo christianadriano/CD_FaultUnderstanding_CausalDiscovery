9f00d1584acbeb4198ab7d49b6b5442971bbf41e
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-10-27 23:11:19 +0100

    Discussion additive and interaction effects to evaluate
    Also generated the adjustments based on paths and intervening variable

--- causal_models/programming_score/CausalInference_ProgrammingScore.R
+++ causal_models/programming_score/CausalInference_ProgrammingScore.R
@@ -36,6 +36,33 @@ paths(dag1.1,c("YoE"),"Score",directed = TRUE)
 # $paths [1] "YoE -> Prof -> Score" "YoE -> Score"        
 # $open [1] TRUE TRUE
 
+adjustmentSets(dag1.1,exposure = "Prof",outcome = "Score",effect = c("direct"))
+#{ YoE } because YoE is a confounder.
+
+"The effect of YoE on Score is influenced by the Profession, i.e., YoE in different
+professions have different effects on Score. In this sense, Profession adjusts the
+effect of YoE on Score. The adjustment can be via mediation and interaction (a.k.a. moderation). 
+
+In the graph model, we can only show mediation. To investigate interaction, we need 
+to build models with interaction. 
+
+The opposite can also be true, i.e., the effect of profession on score is influenced
+by the YoE, which is also a confounder. This means that to intervene in the profession to 
+evaluate Score, I would need to control for YoE. 
+
+What is interesting to know is how much of the effect of YoE on Score is:
+1- additive (direct effect)
+2- additive mediated (indirect effect)
+3- via interacting with profession
+
+Note the assymetry, in a sense that a person cannot easily change Profession, but can increase YoE.
+
+The relation between YoE and Profession is also not given. We need to confirm it.
+I cannot condition on the Score, because it is a collider, which as such, it opens
+the path between YoE and Prof.
+
+"
+
 #Simulate priors
 
 #Build causal models
