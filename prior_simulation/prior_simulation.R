87fb05be5e0a3791bbfe8d666ffd3bae1c01651d
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-11-01 16:30:52 +0100

    Prior to coerced positive relationship

--- prior_simulation/prior_simulation.R
+++ prior_simulation/prior_simulation.R
@@ -1,5 +1,9 @@
 library(rethinking)
 
+"Reset graphics card"
+par(mfrow=c(2,2))
+dev.off()
+
 #Quick way to simulate priors for height (from example 4.14 McElreath)
 sample_mu <- rnorm( 1e4 , 178 , 20 ) #sample from the mean height prior
 sample_sigma <- runif( 1e4 , 0 , 50 ) #sample from the variance height prior
@@ -24,29 +28,15 @@ m5.1 <- quap(
   ) , data = d )
 
 set.seed(10)
-prior <- extract.prior( m5.1 )
+prior <- extract.prior( m5.3 )
 A_seq <- seq( from=-2 , to=2 , length.out=50 )
-mu <- link( m5.1 , post=prior , data=list( A=A_seq ) )
+mu <- link( m5.3 , post=prior , data=list( A=A_seq ) )
 plot( NULL , xlim=c(-2,2) , ylim=c(-2,2),
       xlab="Median Marriage rate (std)",
       ylab="Divorce Rate (std)",)
 title("Prior Simulation")
 
 for ( i in 1:50 ) lines(A_seq, mu[i,] , col=col.alpha("black",0.4))
-#lwd=0.4)
-
-#col=col.alpha("black",0.4))
-
-
-Ax_seq <- seq(from=0,to=0, length.out = 50)
-
-mA <- cbind(Ax_seq,A_seq)
-colnames(mA) <- c("x","y")
-mA_df <- data.frame(mA)
-
-#for ( i in 1:50 ) 
-  lines( mA_df , mu_dff, col=col.alpha("black",0.4)  )
-
 
 m5.2 <- quap(
   alist(
@@ -57,10 +47,19 @@ m5.2 <- quap(
     sigma ~ dexp( 1 )
   ) , data = d )
 
-par(mfrow=c(2,2))
-dev.off()
+m5.3 <- quap(
+  alist(
+    D ~ dnorm( mu , sigma ) ,
+    mu <- a + bA * A ,
+    a ~ dnorm( 0 , 0.2 ) ,
+    bA ~ dlnorm( 0 , 0.5 ) , #forces a positive relationship with median age of marriage to be positive
+    sigma ~ dexp( 1 )
+  ) , data = d )
+
+
+"Log-Normal priors are commonplace. They are an easy way to enforce positive relationships."
+
 
 precis(m5.1)
-precis(m1_2ndOrder)
-plot(coeftab(m1_NoInter,m1_2ndOrder), par=c("a","by"),
+plot(coeftab(m5.1,m5.2,m5.3), par=c("a","bA"),
      xlab="Estimate")
