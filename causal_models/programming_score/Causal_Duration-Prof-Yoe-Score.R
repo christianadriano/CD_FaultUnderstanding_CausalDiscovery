b848085afb30de9d3875c6eb92566b3f6dc394ca
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-11-10 21:43:22 +0100

    Model2 (a[prof] + by*yoe + bt*duration
    slopes zero

--- causal_models/programming_score/Causal_Duration-Prof-Yoe-Score.R
+++ causal_models/programming_score/Causal_Duration-Prof-Yoe-Score.R
@@ -2,3 +2,62 @@
 Causal models that include the duration as covariate
 "
 
+library(rethinking)
+library(stringr)
+library(dplyr)
+
+#Load data
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//load_create_indexes_E2.R")
+
+# standardize variables = (zero centered, standard deviation one)
+#df_E2$yoe <- scale(df_E2$years_programming)
+#df_E2$score <- scale(df_E2$qualification_score)
+
+df_E2$score <-  df_E2$qualification_score
+df_E2$yoe <- df_E2$years_programming
+summary(df_E2$testDuration)
+boxplot(df_E2$testDuration)
+
+df_E2$testDuration_minutes <- df_E2$testDuration/(60 *1000)
+boxplot(df_E2$testDuration_minutes)
+maximum <- 60
+max <- 1625504839/1
+
+#removing outliers (tests above 60 minutes)
+df_E2_aux <- df_E2[df_E2$testDuration_minutes<=60,]
+boxplot(df_E2_aux$testDuration_minutes)
+
+sd(df_E2_aux$testDuration_minutes)
+#>[1] 7.731263
+
+#Model Design
+
+"PRIORS - Regarding the priors, I believe the relationship of the covariates with
+the qualification score is weak. Hence, I am using a prior that considers that 95%
+of the cases (two standard deviations) will be below 2.
+"
+
+
+#Model-2 both, but only additive effects
+# Yoe->Score<-prof
+m2 <- quap(
+  alist(
+    score ~ dnorm( mu , sigma ) ,
+    mu <- a[profession_id]+by*yoe + bt*testDuration_minutes,
+    a[profession_id] ~ dnorm( 0 , 1.0 ),
+    bt ~ dnorm( 0 , 1.0 ) ,
+    by ~ dnorm( 0 , 1.0 ) ,
+    sigma ~ dexp(1)
+  ), data = df_E2
+) 
+precis(m2,2)
+    # mean   sd 5.5% 94.5%
+    # a[1]  4.23 0.03 4.17  4.28
+    # a[2]  4.21 0.04 4.14  4.27
+    # a[3]  4.01 0.05 3.93  4.09
+    # a[4]  3.96 0.04 3.90  4.02
+    # a[5]  4.21 0.07 4.10  4.32
+    # bt    0.00 0.00 0.00  0.00
+    # by    0.00 0.00 0.00  0.01
+    # sigma 0.81 0.01 0.79  0.83
+
