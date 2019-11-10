783312fbbf0cf4af349b9665d2c1cd7c2df60c41
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-11-10 21:47:31 +0100

    Update Causal_Duration-Prof-Yoe-Score.R

--- causal_models/programming_score/Causal_Duration-Prof-Yoe-Score.R
+++ causal_models/programming_score/Causal_Duration-Prof-Yoe-Score.R
@@ -48,16 +48,27 @@ m2 <- quap(
     bt ~ dnorm( 0 , 1.0 ) ,
     by ~ dnorm( 0 , 1.0 ) ,
     sigma ~ dexp(1)
-  ), data = df_E2
+  ), data = df_E2_aux
 ) 
 precis(m2,2)
-    # mean   sd 5.5% 94.5%
-    # a[1]  4.23 0.03 4.17  4.28
-    # a[2]  4.21 0.04 4.14  4.27
-    # a[3]  4.01 0.05 3.93  4.09
-    # a[4]  3.96 0.04 3.90  4.02
-    # a[5]  4.21 0.07 4.10  4.32
-    # bt    0.00 0.00 0.00  0.00
-    # by    0.00 0.00 0.00  0.01
-    # sigma 0.81 0.01 0.79  0.83
+# mean   sd 5.5% 94.5%
+# a[1]  4.20 0.04 4.14  4.26
+# a[2]  4.19 0.04 4.12  4.26
+# a[3]  3.99 0.05 3.90  4.07
+# a[4]  3.93 0.04 3.87  4.00
+# a[5]  4.13 0.07 4.01  4.24
+# bt    0.00 0.00 0.00  0.01
+# by    0.00 0.00 0.00  0.00
+# sigma 0.81 0.01 0.79  0.83
 
+#Interactions
+m3 <- quap(
+  alist(
+    score ~ dnorm( mu , sigma ) ,
+    mu <- a[profession_id] + by[profession_id]*yoe + bt[profession_id]*testDuration_minutes,
+    a[profession_id] ~ dnorm( 0 , 1.0 ),
+    bt ~ dnorm( 0 , 1.0 ) ,
+    by ~ dnorm( 0 , 1.0 ) ,
+    sigma ~ dexp(1)
+  ), data = df_E2_aux
+) 
