a3c48d86eda22ebf8cb13d4b633fa487c40772bb
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-11-15 13:57:18 +0100

    Create Causal_Yoe-Score_E1.R

---
+++ causal_models/programming_score/Causal_Yoe-Score_E1.R
@@ -0,0 +1,11 @@
+"
+Causal Inference Programming Score Experiment-1
+"
+
+library(rethinking)
+library(stringr)
+library(dplyr)
+
+#Load data
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//load_consent_create_indexes_E1.R")
+
