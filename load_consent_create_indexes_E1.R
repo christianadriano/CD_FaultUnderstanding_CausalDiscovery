63d7c1aab1646ac995f5fc0d3c7cb2282854d56f
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-11-15 13:56:57 +0100

    Create load_consent_create_indexes_E1.R

---
+++ load_consent_create_indexes_E1.R
@@ -0,0 +1,29 @@
+"
+Load data from E1 and create indexes for qualification_score
+"
+
+library(farff)
+library(ggplot2)
+library(dplyr)
+
+path <- "C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data//"
+dataset_E1 <- readARFF(paste0(path,"consent_consolidated_Experiment_1.arff"))
+df_E1 <- data.frame(dataset_E1)
+
+head(df_E1)
+
+df <- unique(df)
+
+
+"QUALIFICATION_SCORE"
+
+
+df_E1$qualification_score_label<- factor(df_E1$qualification_score, 
+                                          levels = c(4:0),
+                                          labels = c("100%","75%","50%","25%","0%")
+                                          )
+df_E1$qualification_score_id <- factor(df_E1$qualification_score_label,
+                              levels=levels(df_E1$qualification_score_label),
+                              labels=c(4:0)
+)
+
