10ed5974a8d569754e86cc376e4d71b66d6d3633
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-12-24 23:02:05 +0100

    Create test_item_response_theory.R

---
+++ model_selection/test_item_response_theory.R
@@ -0,0 +1,11 @@
+"
+Exercise on Item Response Theory.
+I am interested in the aspect of choosing the items that make the my 
+classifications mode discriminating. This could be a way to read 
+the results. 
+
+Which configuration of factors have higher discriminating power?
+How to measure discriminating power? 
+
+"
+
