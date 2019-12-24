3a5d2008ac4a886271482bb51403442bd9f21888
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-12-24 23:25:34 +0100

    Update test_item_response_theory.R
    "Plot the information, which tells me which are in the
    x-axis gives me more information in terms of discrimination
    power of the items (all items). This is important to show design the items
    in a way that they focus more or less on certain parameter
    configurations, which in the case of the example is
    ability. See plot."

--- model_selection/test_item_response_theory.R
+++ model_selection/test_item_response_theory.R
@@ -24,11 +24,37 @@ IRT_model <- ltm(LSAT ~ z1, IRT.param=TRUE)
 #   ltm(formula = LSAT ~ z1, IRT.param = TRUE)
 # 
 # Coefficients:
-#   Dffclt  Dscrmn
+#         Dffclt  Dscrmn
 # Item 1  -3.360   0.825
 # Item 2  -1.370   0.723
 # Item 3  -0.280   0.890
 # Item 4  -1.866   0.689
 # Item 5  -3.124   0.657
-# 
+
+#Dffclt = difficulty (x-axis for Median probability)
+#Dscrmn = discrmination (slope, how good the question is in figuring out if
+#are above or below the )
+
+#These Dffclt values show the items are very easy, because they are far away
+#to the negative side. 
+#Recall the logit scale (log-odds = log(p/1-p)) = 
+#-4 (never p=1), #-3 (almost never  p=0.95), 
+#-2 (rarely p=0.88), -1 (p=0.73), 0 (p=0.5)
+
+#Looking at the discrminators, they are very bad too, because they are almost
+#flat. We want discriminators above 1.
 # Log.Lik: -2466.653
+
+plot(IRT_model, type="ICC")
+
+plot(IRT_model, type="ICC", items=c(2,3))
+
+"Plot the information, which tells me which are in the
+x-axis gives me more information in terms of discrimination 
+power of the items (all items). This is important to show design the items
+in a way that they focus more or less on certain parameter
+configurations, which in the case of the example is 
+ability. See plot."
+plot(IRT_model, type="IIC", items=0)
+
+     
