8504572e16f25166451769b0d75103dc11d2b537
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-02-08 23:20:16 +0100

    Create OutlierDetection_E1_E2_Study.R

---
+++ OutlierDetection_E1_E2_Study.R
@@ -0,0 +1,21 @@
+
+"
+Comparative Study of the Outcomes of Traditional Outlier Detection Methods
+
+Goal(Goal-Base Question Metric):
+
+What: Compare the outcomes of different outlier detection methods
+Why-1: mitigate wrong outlier detection might lead incorrect understanding of the data (construct validity threat)
+why-2: increase the reproducibility of the data wrangling procedure
+How: Traditional outlier detection methods (Boxplot, MAD, Adjusted Boxplot)
+Data: Crowdsourced answers from programmers about location of software faults
+
+
+Gist of results:
+Method M1 produced was prefered for dataset D1 because .....
+
+"
+
+install.packages("robustbase")
+library(robustbase)
+
