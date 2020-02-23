eb724b1d3915af083f650b222317dc391856567f
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-02-23 11:32:16 +0100

    Update Verify_CondIndep_E2.R

--- causal_models/difficulty_confidence/Verify_CondIndep_E2.R
+++ causal_models/difficulty_confidence/Verify_CondIndep_E2.R
@@ -1,7 +1,12 @@
 
 "
 Verify the Conditional Independencies implied by a Causal Graph (see below) 
-Data in the context of Experiment-2
+Data in the context of Experiment-2.
+
+This involves fitting regression models.
 
 "
+
+"Load data with treatment field (isBugCovering) and ground truth (answer correct)"
+source("C://Users//Christian//Documents//GitHub//ML_FaultUnderstanding//analysis//descriptive//accuracy//load_apply_ground_truth.R")
 
