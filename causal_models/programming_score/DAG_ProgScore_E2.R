895c7f3c5ded89506968a625ba06c0eb44b9c3ff
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-02-23 19:05:59 +0100

    Create DAG_ProgScore_E2.R

---
+++ causal_models/programming_score/DAG_ProgScore_E2.R
@@ -0,0 +1,30 @@
+"
+Generate and Plot DAG for the E2 programmer score test
+"
+
+library(ggdag)
+library( dagitty )
+library (ggm)
+library(tidyr)
+library(devtools)
+
+dag_score <- ' dag {
+Programmer.Score[Outcome];
+Years.Experience;
+Test.Duration;
+Age;
+Age->Years.Experience;
+Age->Programmer.Score;
+Age->Test.Duration;
+Years.Experience->Programmer.Score;
+Years.Experience->Test.Duration;
+Test.Duration->Programmer.Score
+}'
+
+graph_score <- dagitty(dag_score)
+coordinates(graph_score) <- list( 
+                           x=c(Years.Experience=0,Test.Duration=1,Age=2,Programmer.Score=1) ,
+                           y=c(Years.Experience=0,Test.Duration=1,Age=0,Programmer.Score=2))
+plot(graph_score)
+
+impliedConditionalIndependencies(dag_score)
