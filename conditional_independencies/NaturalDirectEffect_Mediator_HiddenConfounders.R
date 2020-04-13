8c53d7fdc2c46fb4738166e6742da384f6172f4d
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-04-13 20:41:30 +0200

    Update NaturalDirectEffect_Mediator_HiddenConfounders.R

--- conditional_independencies/NaturalDirectEffect_Mediator_HiddenConfounders.R
+++ conditional_independencies/NaturalDirectEffect_Mediator_HiddenConfounders.R
@@ -48,7 +48,35 @@ paths(dag1, from=c("X"),to="Y",directed = TRUE)
 
 #-------------------------------
 #Intervention on Upvotes and Rank -> Effect on Replies
-adjustmentSets(dag1, exposure = c("X","M"), outcome = "Y", 
-               type = c("minimal"), effect = c("total"))
+adjustmentSets(dag1, exposure = c("X"), outcome = "Y", 
+               type = c("minimal"), effect = c("direct"))
 #DIRECT: {length} =>> E[replies|do(upvotes=U,rank=K),length]
 #TOTAL: No Adjustment needed =>> E[replies|do(upvotes=U,rank=K)]
+
+
+
+#---------------------------------------------------------------
+
+dag2 <- dagitty( "dag {
+Y [outcome];
+X [exogenous];
+M [endogenous];
+W2 [exogenous];
+W2_H [hidden_confounder];
+W3 [endogenous];
+X -> Y;
+X -> M;
+W3 -> Y;
+W2 -> X;
+W2_H -> M;
+W2_H -> W2;
+M -> Y
+}")
+
+coordinates(dag2) <- list( x=c(Y=3,X=0,M=1,W2=0,W3=2, W2_H=0),#,W3_H=1) ,
+                           y=c(Y=2,X=2,M=0,W2=1,W3=3,W2_H=0)) #W3_H=4))
+plot( dag2 )
+tidy_dagitty(dag2)
+
+adjustmentSets(dag2, exposure = c("X"), outcome = "Y", 
+               type = c("minimal"), effect = c("total"))
