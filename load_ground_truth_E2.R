ce719b660563c4bb06f7af61a02f451177709f65
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-05-06 23:59:44 +0200

    Update load_ground_truth_E2.R

--- load_ground_truth_E2.R
+++ load_ground_truth_E2.R
@@ -26,20 +26,33 @@ Load Ground Truth to E2 data
 "
 #df_E2 <- readARFF(str_c(path, "//consolidated_Final_Experiment_2.arff"))
 
-df_E2 <- read.csv(str_c(path, "merged_tasks_complexity_E2.csv"))
+#Tasks data with code complexity information
+df_tasks <- read.csv(str_c(path, "merged_tasks_complexity_E2.csv"))
 
-gt2 <- read.csv(str_c(path, "ground_truth_E2.csv"))
+#Score factors computed through IRT Model fitting
+df_irt <- read.csv("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//E2_QualificationTest_IRT.csv")
+df_irt <-  dplyr::select(df_irt, worker_id,z1)
+
+
+#Ground truth for tasks
+df_truth <- read.csv(str_c(path, "ground_truth_E2.csv"))
 
 #Select only the columns that will be left joined with df_E2
-gt2 <-  select(gt2, ID,LineID,isBugCovering,type,faulty_lines,all_Lines,LOC_inspection,LOC_original)
+df_truth <-  dplyr::select(df_truth, ID,LineID,isBugCovering,type,faulty_lines,all_Lines,LOC_inspection,LOC_original)
 
 
 
-"Lef-join microtask_id,ID"
-gt2$ID <- as.factor(gt2$ID) #convert to factor, so I can join with microtask_id column
-df_E2$microtask_id <- as.factor(df_E2$microtask_id)
-df_E2_ground <- left_join(df_E2,gt2,by=c("microtask_id"="ID"))
+"Left-join microtask_id,ID"
+df_truth$ID <- as.factor(df_truth$ID) #convert to factor, so I can join with microtask_id column
+df_tasks$microtask_id <- as.factor(df_tasks$microtask_id)
+df_tasks$worker_id <- as.factor(df_tasks$worker_id)
+df_E2_ground <- left_join(df_tasks,df_truth,by=c("microtask_id"="ID"))
 
+"Left-join worker_id, worker_id"
+df_irt$worker_id <- as.factor(df_irt$worker_id) #convert to factor, so I can join with microtask_id column
+df_E2_ground$worker_id <- as.factor(df_E2_ground$worker_id) #convert to factor, so I can join with microtask_id column
+df_E2_ground <- inner_join(df_E2_ground,df_irt,by=c("worker_id"="worker_id"))
+#dim(df_E2_ground) [1] 1462   54 Should have 2580 rows!
 
 "
 Apply Ground Truth to E2 data
