adafd55445c76fe9ffa7eba644f330fffc7bede2
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-09-12 22:52:51 +0200

    Update PropensityScore_Matching_E2.R

--- matching/PropensityScore_Matching_E2.R
+++ matching/PropensityScore_Matching_E2.R
@@ -16,7 +16,7 @@ data(lalonde)
 #df <- scale(lalonde[c("age", "educ", "re74", "re75"),])
 
 df <- lalonde %>% mutate_at(
-                      c("age", "educ", "re74", "re75"), 
+                      c("age", "educ","married", "re74", "re75","re78"), 
                       ~(scale(.) %>% as.vector)
                       ) 
 
@@ -28,4 +28,21 @@ mean_Edu_diff <-  mean(df_married[df_married$treat==1,]$edu) -  mean(df_married[
 mean_re74_diff <-  mean(df_married[df_married$treat==1,]$re74) -  mean(df_married[df_married$treat==0,]$re74)
 mean_re75_diff <-  mean(df_married[df_married$treat==1,]$re75) -  mean(df_married[df_married$treat==0,]$re75)
 
-mean_Age_diff + mean_Edu_dif
+mean_Age_diff + mean_Edu_dif + mean_re74_diff + mean_Edu_re75
+
+married_1 = df[df$treat==1,]$married
+married_0 = df[df$treat==0,]$married
+
+avg_married_1 = mean(married_1)
+avg_married_0 = mean(married_0)
+var_married_1 = var(married_1)
+var_married_0 = var(married_0)
+
+#difference between the means divided by the square-root of the sum
+#sample variances divided by 2
+(avg_married_0 - avg_married_1) / (sqrt((var_married_0+var_married_1)/2))
+#[1] 0.719492
+
+
+mean(lalonde[lalonde$treat==1,]$re78) - mean(lalonde[lalonde$treat==0,]$re78)
+mean(df$married)
