7ae0b019f7732e83e3b27575571bf6565346ac04
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-09-21 23:59:33 +0200

    Update IPTW_example.R

--- iptw/IPTW_example.R
+++ iptw/IPTW_example.R
@@ -9,3 +9,10 @@ library(survey)
 
 load(url("http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/rhc.sav"))
 View(rhc)
+
+#create a dataset only with the following variables and convert them to numeric
+
+ARF <- as.numeric(rfc$cat1=="ARF")
+CHF <- as.numeric(rfc$cat1=="CHF")
+Cirr <- as.numeric(rfc$cat1=="Cirrhoris")
+colcan <- as.numeric(rfc$cat1=="Colon Cancer")
