100e481c65a155c2541aac09ec010e7674337fca
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-09-23 22:09:10 +0200

    Create Imbalance_SMD_BugNoBug_E2.R

---
+++ matching/Imbalance_SMD_BugNoBug_E2.R
@@ -0,0 +1,10 @@
+"Check imbalances in Standardized Mean Differences - SMD
+
+plotting results with Cobalt
+https://cran.rstudio.com/web/packages/cobalt/vignettes/cobalt_A4_love.plot.html
+
+Treatment: Bug or NoBug
+Analyze imbalance for each Task (HIT01 to HIT08)
+"
+
+
