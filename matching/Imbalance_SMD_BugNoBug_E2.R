f78e3cd0208ff8ce66ed56ec0a07a837ecf44e1f
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-09-26 21:40:32 +0200

    Update Imbalance_SMD_BugNoBug_E2.R

--- matching/Imbalance_SMD_BugNoBug_E2.R
+++ matching/Imbalance_SMD_BugNoBug_E2.R
@@ -7,4 +7,8 @@ Treatment: Bug or NoBug
 Analyze imbalance for each Task (HIT01 to HIT08)
 "
 
+"Load data with treatment field (isBugCovering) and ground truth (answer correct)"
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_ground_truth_E2.R")
 
+
+summary(df_E2_ground)
