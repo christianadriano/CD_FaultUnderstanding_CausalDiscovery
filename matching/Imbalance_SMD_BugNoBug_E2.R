920ecbf1c1d4b06f07e16a52c00ad946183ccff5
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-09-30 23:54:53 +0200

    Update Imbalance_SMD_BugNoBug_E2.R

--- matching/Imbalance_SMD_BugNoBug_E2.R
+++ matching/Imbalance_SMD_BugNoBug_E2.R
@@ -1,10 +1,11 @@
 "Check imbalances in Standardized Mean Differences - SMD
 
-plotting results with Cobalt
-https://cran.rstudio.com/web/packages/cobalt/vignettes/cobalt_A4_love.plot.html
 
 Treatment: Bug or NoBug
 Analyze imbalance for each Task (HIT01 to HIT08)
+plotting results with Cobalt
+https://cran.rstudio.com/web/packages/cobalt/vignettes/cobalt_A4_love.plot.html
+
 "
 
 "Load data with treatment field (isBugCovering) and ground truth (answer correct)"
