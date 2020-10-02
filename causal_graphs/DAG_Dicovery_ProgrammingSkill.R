fe910727d45e13234788d912dcd69b31846a321d
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-10-02 23:56:03 +0200

    Update DAG_Dicovery_ProgrammingSkill.R

--- causal_graphs/DAG_Dicovery_ProgrammingSkill.R
+++ causal_graphs/DAG_Dicovery_ProgrammingSkill.R
@@ -1,20 +1,40 @@
 
 "
-Causal discovery for the programming skill related variables
+Causal discovery for the programming skill related factors
+
+"  
+"There are two packages in R for Bayesian Network Learning (or causal graph learning):
+bnlearn and pcalg. I decided to use bnlearn, because its installation was easier than
+pcalg. The pcalg requires the BiocManager (install.packages(BiocManager)), which is
+very larger framework, which is not installing smoothly"
+
+#-----------------------------------
+#Example with BNLEARN (will move it to the bottom later)
+#install.packages("bnlearn")
+#
+library(bnlearn)
+data(learning.test)
+str(learning.test)
+bn.gs <- gs(learning.test)
+bn.gs
+plot(bn.gs)
+#-----------------------------------            
+"APPLY TO THE PROGRAMMING SKILL FACTORS 
 
 profession [exogenous];
 years_programming [exogenous];
 age [exogenous]
+gender [exogenous]
 qualification_score [outcome];
 file_name [exogenous] work as block, because programmers were tested for each file_name
 need to consider unique worker-id, i.e., programmer took a single qualification test, even
 if they have taken multiple tasks.
-"  
-install.packages("pcalg")
-install.packages("graph")
-library(pcalg)
+" 
+install.packages("tidyverse")
+"Load data with treatment field (isBugCovering) and ground truth (answer correct)"
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_ground_truth_E2.R")
+#summary(df_E2_ground)
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//create_indexes_E2.R")
 
-BiocManager::install("graph")
+df_E2_ground<- run(df_E2_ground)
