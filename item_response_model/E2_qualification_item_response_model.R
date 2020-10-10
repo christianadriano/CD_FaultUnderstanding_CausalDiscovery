bb1e45e06d1fc926f1c88f8d3f7dda1b1eb19b0a
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-10-11 00:28:08 +0200

    Update E2_qualification_item_response_model.R

--- item_response_model/E2_qualification_item_response_model.R
+++ item_response_model/E2_qualification_item_response_model.R
@@ -13,13 +13,13 @@ df_E2 <- df_consent[complete.cases(df_consent[,"qualification_score"]),]
 #Original size: 3657   30 , new size 1438 30
 
 "Replace false for 0(zero) and true for one(1)"
-df_E2$test1_ <-  ifelse(df_E2$test1=="true",1,0)
-df_E2$test2_ <-  ifelse(df_E2$test2=="true",1,0)
-df_E2$test3_ <-  ifelse(df_E2$test3=="true",1,0)
-df_E2$test4_ <-  ifelse(df_E2$test4=="true",1,0)
-df_E2$test5_ <-  ifelse(df_E2$test5=="true",1,0)
+# df_E2$test1_ <-  ifelse(df_E2$test1=="true",1,0)
+# df_E2$test2_ <-  ifelse(df_E2$test2=="true",1,0)
+# df_E2$test3_ <-  ifelse(df_E2$test3=="true",1,0)
+# df_E2$test4_ <-  ifelse(df_E2$test4=="true",1,0)
+# df_E2$test5_ <-  ifelse(df_E2$test5=="true",1,0)
 
-df <- df_E2 %>% dplyr::select(test1_,test2_,test3_,test4_,test5_)
+df <- df_E2 %>% dplyr::select(test1,test2,test3,test4,test5)
 
 write.csv(df,"C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//E2_QualificationTestResults.csv")
 
@@ -28,12 +28,12 @@ IRT_model <- ltm(df ~ z1, IRT.param=TRUE)
 
 IRT_model
 # Coefficients:
-#          Dffclt  Dscrmn
-# test1_   0.126   2.997
-# test2_   0.173   1.572
-# test3_   0.330   2.067
-# test4_   0.071   0.700
-# test5_   0.606   1.598
+#         Dffclt  Dscrmn
+# test1   0.018   3.230
+# test2   0.022   1.598
+# test3   0.233   2.386
+# test4  -0.162   0.809
+# test5   0.417   1.602
 
 "Coefficients for dffclt show the test was not so difficult. 
 
@@ -61,42 +61,43 @@ factors <- factor.scores.ltm(IRT_model)
 factors
 # Scoring Method: Empirical Bayes
 # Factor-Scores for observed response patterns:
-#   test1_ test2_ test3_ test4_ test5_ Obs     Exp     z1 se.z1
-# 1       0      0      0      0      0 241 287.560 -0.898 0.659
-# 2       0      0      0      0      1  46  32.376 -0.375 0.501
-# 3       0      0      0      1      0 145 143.202 -0.633 0.575
-# 4       0      0      0      1      1  27  23.433 -0.214 0.464
-# 5       0      0      1      0      0  77  35.402 -0.264 0.474
-# 6       0      0      1      0      1  12  10.393  0.054 0.428
-# 7       0      0      1      1      0  18  27.936 -0.117 0.447
-# 8       0      0      1      1      1   4  10.582  0.180 0.423
-# 9       0      1      0      0      0  79  65.801 -0.382 0.502
-# 10      0      1      0      0      1  14  15.896 -0.038 0.436
-# 11      0      1      0      1      0  76  47.385 -0.219 0.465
-# 12      0      1      0      1      1  28  15.012  0.091 0.426
-# 13      0      1      1      0      0  12  20.840  0.049 0.428
-# 14      0      1      1      0      1   3  10.687  0.338 0.429
-# 15      0      1      1      1      0   6  21.135  0.175 0.423
-# 16      0      1      1      1      1   7  13.830  0.470 0.442
-# 17      1      0      0      0      0  58  38.416 -0.072 0.440
-# 18      1      0      0      0      1  16  15.773  0.221 0.424
-# 19      1      0      0      1      0  54  35.324  0.060 0.427
-# 20      1      0      0      1      1  14  18.477  0.348 0.430
-# 21      1      0      1      0      0  30  23.896  0.306 0.427
-# 22      1      0      1      0      1   5  20.303  0.618 0.464
-# 23      1      0      1      1      0  22  30.097  0.436 0.438
-# 24      1      0      1      1      1  23  33.377  0.778 0.495
-# 25      1      1      0      0      0  22  31.463  0.217 0.423
-# 26      1      1      0      0      1  12  22.336  0.515 0.448
-# 27      1      1      0      1      0  15  36.712  0.343 0.429
-# 28      1      1      0      1      1  24  33.648  0.663 0.472
-# 29      1      1      1      0      0  50  40.003  0.612 0.463
-# 30      1      1      1      0      1  59  64.039  1.015 0.549
-# 31      1      1      1      1      0  50  65.450  0.772 0.494
-# 32      1      1      1      1      1 189 147.216  1.249 0.609
+#     test1 test2 test3 test4 test5 Obs   Exp     z1 se.z1
+# 1      0     0     0     0     0 271 320.220 -0.966 0.642
+# 2      0     0     0     0     1  56  41.950 -0.475 0.481
+# 3      0     0     0     1     0 159 164.245 -0.683 0.546
+# 4      0     0     0     1     1  34  32.191 -0.305 0.438
+# 5      0     0     1     0     0  80  31.855 -0.310 0.440
+# 6      0     0     1     0     1  12  11.481 -0.033 0.400
+# 7      0     0     1     1     0  19  28.424 -0.164 0.413
+# 8      0     0     1     1     1   4  13.259  0.095 0.397
+# 9      0     1     0     0     0  91  79.162 -0.476 0.481
+# 10     0     1     0     0     1  21  21.535 -0.163 0.413
+# 11     0     1     0     1     0  90  60.698 -0.306 0.439
+# 12     0     1     0     1     1  51  22.033 -0.030 0.400
+# 13     0     1     1     0     0  12  21.621 -0.034 0.400
+# 14     0     1     1     0     1   3  12.806  0.220 0.402
+# 15     0     1     1     1     0   7  24.954  0.094 0.397
+# 16     0     1     1     1     1   9  18.947  0.355 0.417
+# 17     1     0     0     0     0  64  40.740 -0.158 0.413
+# 18     1     0     0     0     1  17  19.213  0.100 0.397
+# 19     1     0     0     1     0  69  41.880 -0.025 0.400
+# 20     1     0     0     1     1  19  25.218  0.229 0.403
+# 21     1     0     1     0     0  31  24.556  0.225 0.403
+# 22     1     0     1     0     1   6  24.412  0.507 0.443
+# 23     1     0     1     1     0  31  36.512  0.361 0.418
+# 24     1     0     1     1     1  37  48.471  0.679 0.481
+# 25     1     1     0     0     0  24  36.160  0.100 0.397
+# 26     1     1     0     0     1  15  27.767  0.362 0.418
+# 27     1     1     0     1     0  22  47.432  0.228 0.403
+# 28     1     1     0     1     1  34  47.495  0.511 0.444
+# 29     1     1     1     0     0  58  45.856  0.506 0.443
+# 30     1     1     1     0     1  82  83.555  0.881 0.534
+# 31     1     1     1     1     0  71  90.979  0.678 0.481
+# 32     1     1     1     1     1 289 242.373  1.144 0.610
+
 
 "Factor scores shows tha the most frequent combination (Obs) were
-all wrong (241), all correct (189), and only test_4 correct (145)
+all wrong (271), all correct (289), and only test_4 correct (159)
 "
 
 hist(factors$score.dat$z1 )
@@ -107,8 +108,15 @@ hist(df_E2$qualification_score)
 
 df_score.dat <- data.frame(factors$score.dat)
 
+#Convert to double to be able to join with the fields tes1..5 of df_score.dat
+df_score.dat$test1 <- as.factor(df_score.dat$test1)
+df_score.dat$test2 <- as.factor(df_score.dat$test2)
+df_score.dat$test3 <- as.factor(df_score.dat$test3)
+df_score.dat$test4<- as.factor(df_score.dat$test4)
+df_score.dat$test5 <- as.factor(df_score.dat$test5)
+
 #LEFT JOIN to associate the new difficulty scores (z1) to the partipants.
-df_new <- left_join(df_E2,df_score.dat,by=c("test1_"="test1_","test2_"="test2_","test3_"="test3_","test4_"="test4_","test5_"="test5_"))
+df_new <- left_join(df_E2,df_score.dat,by=c("test1"="test1","test2"="test2","test3"="test3","test4"="test4","test5"="test5"))
 
 
 #Store in the original file the new difficulty scores (z1) of the partipants
@@ -119,6 +127,13 @@ plot(df_new$years_programming, df_new$z1)
 title("Factor Scores by Years of Programming - E2")
 plot(df_new$years_programming, df_new$qualification_score)
 title("Average Scores by Years of Programming - E2")
+
+cor.test(df_new$years_programming, df_new$z1)
+#cor=0.3082745, t = 13.695, df = 1786, p-value < 2.2e-16
+cor.test(df_new$years_programming, df_new$qualification_score)
+#cor =0.321539 t = 14.351, df = 1786, p-value < 2.2e-16
+
+#Correlation did not change and continue to be significant
 
 #----------------------------------------------------------------
 
