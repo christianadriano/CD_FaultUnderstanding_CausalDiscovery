c6df4f1be9cd22c891087dcb11a689b85533d422
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-10-15 00:01:33 +0200

    Update load_consent_create_indexes_E2.R

--- data_loaders/load_consent_create_indexes_E2.R
+++ data_loaders/load_consent_create_indexes_E2.R
@@ -3,30 +3,30 @@ Load CONSENT data from E2 and create indexes for: profession, qualification_scor
 
 "
 library(farff)
+library(readr)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
-
-#What is the difference between:
-# "consent_consolidated_Experiment_2.arff"  [1] 3658   21 has everyone
-#  consolidated_Final_Experiment_2.arff" [1] 2580   32 only has the ones who passed the test
-#There is a BUG in this file consolidated_Final_Experiment_2.arff 
-#this worker_id 3AI7C4g38-9_3 should not have tasks associated!
 
 path <- "C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data//"
 #path <- "C://Users//Christian//Documents//GitHub//DW_Microtasks//output//"
 dataset_E2 <- readARFF(paste0(path, "consent_consolidated_Experiment_2.arff"))
 df_consent <- data.frame(dataset_E2)
-dim(df_consent)
+dim(df_consent) 
 
-#file_path <-
-#  "C://Users//Christian//Documents//GitHub//ML_FaultUnderstanding//data//consolidated_Final_Experiment_2.arff"
-#df_consolidated_final <-  readARFF(file_path)
+#Filter-out rows without test data
+dim(df_consent[is.na(df_consent$test1),]) #1870 are NA
+df_consent <- df_consent[!is.na(df_consent$test1),]
+dim(df_consent) #1788 are not NA.
 
-
-#source("C://Users//Christian//Documents//GitHub//ML_FaultUnderstanding//analysis//descriptive//accuracy//load_apply_ground_truth.R")
-#data is loaded on df2_ground
-#df_consent <- df2_ground
+#Analyzing workers who took the test multiple times. am keeping the all the data for people who took the test multiple times, 
+#I will take the average of the tests.
+#Discover who took multiple times: worker_id's with more than one file_name
+unique_workers <- unique(df_consent$worker_id) #1698 unique workers with tests
+df_tb <- data.frame(table(df_consent$worker_id))
+colnames(df_tb) <- c("worker_id","count")
+dim(df_tb[df_tb$count>1,]) #76 workers took the test more than once
+multiple_takers <- df_tb[df_tb$count>1,"worker_id"]
 
 #Score factors computed through IRT Model fitting
 df_irt <- read.csv("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data//irt//E2_QualificationTest_IRT.csv")
