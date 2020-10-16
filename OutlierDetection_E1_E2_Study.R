11b2295e595f2c1d44ad9d687c90f5b51145eb95
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-10-16 21:13:24 +0200

    Update OutlierDetection_E1_E2_Study.R

--- OutlierDetection_E1_E2_Study.R
+++ OutlierDetection_E1_E2_Study.R
@@ -16,6 +16,40 @@ Method M1 produced was prefered for dataset D1 because .....
 
 "
 
-install.packages("robustbase")
-library(robustbase)
+#install.packages("robustbase")
+#library(robustbase)
+install.packages("outliers")
+library(outliers)
+library(farff)
+
+path <- "C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data//"
+dataset_E2 <- readARFF(paste0(path, "consent_consolidated_Experiment_2.arff"))
+df_E2 <- data.frame(dataset_E2)
+df_consent <- data.frame(dataset_E2)
+dim(df_consent) #3658
+
+#Processing Age, so remove rows where Age==NA
+df_consent$experience <- as.factor(df_consent$experience)
+df_consent_age <- df_consent[!is.na(df_consent$age),]
+dim(df_consent_age)  #2463
+
+#GRUBBS TEST - https://www.statsandr.com/blog/outliers-detection-in-r/
+outliers::grubbs.test(df_consent_age[df_consent_age$experience=="Undergraduate_Student",]$age)
+#100 is an outlier
+
+#substitute the outlier for the median value
+df_consent_age[df_consent$experience=="Undergraduate_Student" &
+             df_consent_age$age==100,]$age <- median(df_consent_age[
+                                                  df_consent_age$experience=="Undergraduate_Student","age"])
+
+
+outliers::grubbs.test(df_consent[df_consent$experience=="Professional_Developer",]$age)
+#57 is an outlier
+
+outliers::grubbs.test(df_consent[df_consent$experience=="Graduate_Student",]$age)
+#57 is an outlier
+
+outliers::grubbs.test(df_consent[df_consent$experience=="Hobbyist",]$age)
+#66 is an outlier
+
 
