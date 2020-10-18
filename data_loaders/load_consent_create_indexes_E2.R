73160244e54718345df1d8a6d86aebf0b3c580c5
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-10-18 17:23:14 +0200

    Update load_consent_create_indexes_E2.R

--- data_loaders/load_consent_create_indexes_E2.R
+++ data_loaders/load_consent_create_indexes_E2.R
@@ -1,5 +1,6 @@
 "
-Load CONSENT data from E2 and create indexes for: profession, qualification_score, file_name, country
+Load CONSENT data from E2 and create indexes for: profession, 
+qualification_score, file_name, country
 
 "
 library(farff)
@@ -19,14 +20,12 @@ dim(df_consent[is.na(df_consent$test1),]) #1870 are NA
 df_consent <- df_consent[!is.na(df_consent$test1),]
 dim(df_consent) #1788 are not NA.
 
-#Analyzing workers who took the test multiple times. am keeping the all the data for people who took the test multiple times, 
-#I will take the average of the tests.
-#Discover who took multiple times: worker_id's with more than one file_name
-unique_workers <- unique(df_consent$worker_id) #1698 unique workers with tests
-df_tb <- data.frame(table(df_consent$worker_id))
-colnames(df_tb) <- c("worker_id","count")
-dim(df_tb[df_tb$count>1,]) #76 workers took the test more than once
-multiple_takers <- df_tb[df_tb$count>1,"worker_id"]
+df_consent <- as_tibble(df_consent)
+df_consent <- rename(df_consent,profession=experience)
+
+#remove rows without profession information
+df_consent <- df_consent[!is.na(df_consent$profession),] #left with 2463
+dim(df_consent) #2463
 
 #Score factors computed through IRT Model fitting
 df_irt <- read.csv("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data//irt//E2_QualificationTest_IRT.csv")
