2e921785899a651520700d9804bb8f41f52652f0
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-10-21 23:50:33 +0200

    Update impact_IRT_adjustment.R

--- data_loaders/impact_IRT_adjustment.R
+++ data_loaders/impact_IRT_adjustment.R
@@ -17,3 +17,22 @@ library(tidyverse)
 #Load only Consent data. No data from tasks, only from demographics and qualification test
 source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E2.R")
 df_E2_ground<- df_consent
+
+scale_01 <- function(x){(x-min(x))/(max(x)-min(x))}
+
+df_consent$qualification_score_scaled <- scale_01(df_consent$qualification_score)
+df_consent$z1_scaled <- scale_01(df_consent$z1)
+
+
+summary(df_consent$qualification_score_scaled)
+summary(df_consent$z1_scaled)
+
+df_consent$decreased_score <- df_consent$qualification_score_scaled>df_consent$z1_scaled
+
+df_test <- df_consent[df_consent$decreased_score==TRUE,]
+
+table(df_test$profession)
+# Professional            Programmer              Hobbyist      Graduate_Student 
+# 138                    19                   152                    82 
+# Undergraduate_Student                 Other 
+# 152                    37 
