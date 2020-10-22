621ce0a7051b27ddd007cfe8bea3bbd8ac3c4d19
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-10-22 20:48:59 +0200

    Update compare_professions_yoe_age_E2.R

--- descriptive_statistics/compare_professions_yoe_age_E2.R
+++ descriptive_statistics/compare_professions_yoe_age_E2.R
@@ -58,3 +58,45 @@ Concerning outliers, except for Programmers, all other professions have a simila
 outliers counts. The only distinction is the outliers for Students are below 
 20 years of experience, whereas the "Other" go up to 35 years. This makes sense, 
 because students are supposed to be among the youngest  study cohorts."
+
+#----------------------------------------------------------------
+#AGE
+
+df_consent %>%
+  mutate(text = fct_reorder(profession,age, .desc = TRUE)) %>%
+  ggplot( aes(x=age)) +
+  geom_density(alpha=0.6, color="darkgrey", fill="lightblue") +
+  theme_minimal()+
+  theme(
+    legend.position="none",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 12),
+    plot.title = element_text(size=14),
+    axis.text.x = element_text(hjust = 1, size=12)
+  ) +
+  xlab("Age (years)") +
+  ylab("Density (%)") +
+  facet_wrap(~text,nrow=3,ncol=2)+
+  ggtitle("Age of Participants across Professions") 
+
+"We can see in the density plots that groups show similar shapes of distribution. 
+I would say that Programmer is a more dispersed version of professional,
+and the same of Hobbyists with relation to Graduates and Others to Undergrads"
+
+#Boxplots
+df_consent %>%
+  ggplot( aes(y=age, x=reorder(profession,1/age))) +
+  geom_boxplot()+
+  geom_smooth(method = "lm", se=FALSE, fullrange = TRUE, color="steelblue",  linetype="dashed", aes(group=1))+
+  stat_summary(fun=mean, geom="point", shape=4, size=3)+
+  theme_minimal()+
+  theme(
+    legend.position="none",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 12),
+    plot.title = element_text(size=14),
+    axis.text.x = element_text(angle = 20, hjust = 1, size=12)
+  ) +
+  xlab("Profession") +
+  ylab("Age (years)") +
+  ggtitle("Age of Participants across Professions") 
