78a7a3539c74ceb92c083309ffccc3de07397de6
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-10-22 14:24:11 +0200

    Update impact_IRT_adjustment.R

--- data_loaders/impact_IRT_adjustment.R
+++ data_loaders/impact_IRT_adjustment.R
@@ -10,7 +10,6 @@ the ones who got easier questions correct, but the difficult ones wrong.
 "
 library(ggplot2)
 library(dplyr)
-library(hrbrthemes)
 library(tidyr)
 library(tidyverse)
 
@@ -23,7 +22,6 @@ scale_01 <- function(x){(x-min(x))/(max(x)-min(x))}
 df_consent$qualification_score_scaled <- scale_01(df_consent$qualification_score)
 df_consent$z1_scaled <- scale_01(df_consent$z1)
 
-
 summary(df_consent$qualification_score_scaled)
 summary(df_consent$z1_scaled)
 
@@ -31,13 +29,13 @@ df_consent$decreased_score <- df_consent$qualification_score_scaled>df_consent$z
 
 df_test <- df_consent[df_consent$decreased_score==TRUE,]
 
-df_descrease <- data.frame(table(df_test$profession))
+df_descreased <- data.frame(table(df_test$profession))
 # Professional            Programmer              Hobbyist      Graduate_Student 
 # 138                    19                   152                    82 
 # Undergraduate_Student                 Other 
 # 152     37 
 
-colnames(df_descrease) <- c("profession","decreased")
+colnames(df_descreased) <- c("profession","decreased")
 
 df_original <- data.frame(table(df_consent$profession))
 # 417                    49                   484                   283 
@@ -45,30 +43,30 @@ df_original <- data.frame(table(df_consent$profession))
 # 443                   112 
 colnames(df_original) <- c("profession","original")
 
-joined <- left_join(df_original,df_descrease,by="profession")
-joined$proportion <- (joined$original-joined$decreased)/joined$original
+joined <- left_join(df_original,df_descreased,by="profession")
+joined$proportion <- round((joined$original-joined$decreased)/joined$original, 2)
 
 ggplot(data=joined, aes(x=reorder(profession,proportion), y=proportion))+
-  geom_bar(stat="identity")+
-#  theme_ipsum_pub()+
+  geom_bar(stat="identity", fill="steelblue")+
+  geom_text(aes(label=reorder(proportion,profession)), vjust=1.6, color="white", size=3.5)+
+  theme_minimal()+
   theme(
     legend.position="none",
     panel.spacing = unit(0.1, "lines"),
     strip.text.x = element_text(size = 10),
-    panel.grid=element_blank(),
     plot.title = element_text(size=12),
     axis.text.x = element_text(angle = 25, hjust = 1, size=10)
   ) +
   xlab("Profession") +
-  ylab("Reduction in score") +
-  ggtitle("Proportion that reduced their score after adjustment by IRT model") 
+  ylab("Proportion of subjects") +
+  ggtitle("Proportion of subjects whose score decreased after adjustment by IRT model") 
 
 #Difference in reduction (taking the smallest score proportion as baseline)
 smallest_score_proportion <- min(joined$proportion)
 joined$differences <- round((joined$proportion-smallest_score_proportion) *100,2)
 
 ggplot(data=joined, aes(x=reorder(profession,differences), y=differences))+
-  geom_bar(stat="identity", fill="darkgrey")+
+  geom_bar(stat="identity", fill="steelblue")+
   geom_text(aes(label=reorder(differences,profession)), vjust=1.6, color="white", size=3.5)+
   theme_minimal()+
   theme(
@@ -85,6 +83,54 @@ ggplot(data=joined, aes(x=reorder(profession,differences), y=differences))+
 "
 Programmers and Undergraduates were the most affected in numbers. 
 The less affected were the Graduate students and Hobbyists, which is surprising.
-Still need to compute the magnitude to the decrease.
+"
+
+#------------------------------------------------------------------------
+#Now I investigate the magnitude of the individual adjustment in score
+
+df_consent$magnitude_adjustment <- df_consent$z1_scaled - df_consent$qualification_score_scaled
+"if the magnitud_adjustment is negative, then it means that the score decreased. 
+Conversely, if magnitud_adjustment is positive, then the score increased."
+
+#plot the distribution of these magnitutes by profession
+df_consent %>%
+  ggplot( aes(y=magnitude_adjustment, x=reorder(profession,1-magnitude_adjustment))) +
+  geom_boxplot()+
+  geom_smooth(method = "lm", se=FALSE, fullrange = TRUE, color="steelblue",  linetype="dashed", aes(group=1))+
+  stat_summary(fun=mean, geom="point", shape=4, size=3)+
+  theme_ipsum_pub()+
+  theme(
+    legend.position="none",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 10),
+    panel.grid=element_blank(),
+    plot.title = element_text(size=12),
+    axis.text.x = element_text(angle = 25, hjust = 1, size=10)
+  ) +
+  #facet_wrap(~text)+
+  ylab("Magnitude of score adjustment") +
+  xlab("Profession") +
+  ggtitle("Magnitude of score adjustment across Professions") 
 
 "
+We have two suprising items here.
+1- The adjustment was positive for all, except Programmers
+2- We can see that although Programmer and Professional were the among the one with 
+highest number of people affected by adjustement (Previous barchart), 
+the average magnitude of their adjustment were the two top lowest.
+"
+
+mean(df_consent[df_consent$profession=="Professional",]$magnitude_adjustment)
+mean(df_consent[df_consent$profession=="Programmer",]$magnitude_adjustment)
+mean(df_consent[df_consent$profession=="Hobbyist",]$magnitude_adjustment)
+mean(df_consent[df_consent$profession=="Other",]$magnitude_adjustment)
+mean(df_consent[df_consent$profession=="Undergraduate_Student",]$magnitude_adjustment)
+mean(df_consent[df_consent$profession=="Graduate_Student",]$magnitude_adjustment)
+
+"
+However, doing an Omnibus test, we could not show any statistically significant distinctions
+on these average of adjustments.
+"
+two.way <- aov(magnitude_adjustment ~ profession,data=df_consent)
+summary(two.way)
+TukeyHSD(two.way)
