6b0cd953e16637900dcbcb1700e5c566e4514451
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-11-23 00:22:30 +0100

    Update Imbalance_SMD_Qualification_E2.R

--- matching/Imbalance_SMD_Qualification_E2.R
+++ matching/Imbalance_SMD_Qualification_E2.R
@@ -46,145 +46,158 @@ raw_table_all <- CreateTableOne(vars=xvars,strata="is_fast",data=df_selected,tes
 print(raw_table_all, smd=TRUE)
 
 #Data is imbalanced as the SMD is larger than 0.1
-# Stratified by isBugCovering
-#                                    TRUE            FALSE           SMD   
-# n                                  278            1184                
-# years_programming (mean (SD))    10.78 (10.71)    8.72 (8.63)    0.212
-# qualification_score (mean (SD))   4.23 (0.79)     4.13 (0.79)    0.127
-# volume_Halstead (mean (SD))     299.43 (437.96) 116.17 (137.63)  0.565
-# profession (%)                                                   0.149
-# Professional_Developer          120 (43.2)      474 (40.0)         
-# Hobbyist                         55 (19.8)      229 (19.3)         
-# Graduate_Student                 32 (11.5)      149 (12.6)         
-# Undergraduate_Student            41 (14.7)      231 (19.5)         
-# Other                            30 (10.8)      101 ( 8.5)    
+# Stratified by is_fast
+#                                 FALSE         TRUE          SMD   
+# n                               150          1638               
+# years_programming (mean (SD))  2.82 (3.19)   5.72 (6.84)   0.544
+# age (mean (SD))               27.61 (7.32)  30.46 (8.70)   0.354
+# test_duration (mean (SD))      1.31 (0.70)   5.47 (3.26)   1.763
+# profession (%)                                             0.898
+# Graduate_Student              23 (15.3)    260 (15.9)        
+# Hobbyist                      44 (29.3)    440 (26.9)        
+# Other                         11 ( 7.3)    101 ( 6.2)        
+# Professional                   0 ( 0.0)    417 (25.5)        
+# Programmer                    13 ( 8.7)     36 ( 2.2)        
+# Undergraduate_Student         59 (39.3)    384 (23.4)    
 
 #HIT01_8
 df_file_name <- df_selected[df_selected$file_name=="HIT01_8",]
 raw_table <- CreateTableOne(vars=xvars,strata="is_fast",data=df_file_name,test=FALSE)
 print(raw_table, smd=TRUE)
-#                                 Stratified by isBugCovering
-#                                   TRUE           FALSE         SMD   
-# n                                   27           103               
-# years_programming (mean (SD))    11.93 (11.29)  9.16 (9.37)   0.267
-# qualification_score (mean (SD))   4.15 (0.77)   4.17 (0.79)   0.022
-# volume_Halstead (mean (SD))     101.74 (44.29) 95.70 (74.35)  0.099
-# profession (%)                                                0.437
-# Professional_Developer           11 (40.7)     39 (37.9)        
-# Hobbyist                          4 (14.8)     14 (13.6)        
-# Graduate_Student                  5 (18.5)     12 (11.7)        
-# Undergraduate_Student             4 (14.8)     32 (31.1)        
-# Other                             3 (11.1)      6 ( 5.8)  
+# Stratified by is_fast
+#                                 FALSE         TRUE          SMD   
+# n                                12           131               
+# years_programming (mean (SD))  5.50 (5.65)   5.78 (6.22)   0.048
+# age (mean (SD))               27.75 (4.49)  29.76 (8.56)   0.294
+# test_duration (mean (SD))      1.39 (1.00)   5.69 (3.59)   1.631
+# profession (%)                                             1.090
+# Graduate_Student               1 ( 8.3)     18 (13.7)        
+# Hobbyist                       3 (25.0)     28 (21.4)        
+# Other                          2 (16.7)     10 ( 7.6)        
+# Professional                   0 ( 0.0)     38 (29.0)        
+# Programmer                     2 (16.7)      3 ( 2.3)        
+# Undergraduate_Student          4 (33.3)     34 (26.0)  
 
 
 df_file_name <- df_selected[df_selected$file_name=="HIT02_24",]
 raw_table <- CreateTableOne(vars=xvars,strata="is_fast",data=df_file_name,test=FALSE)
 print(raw_table, smd=TRUE)
-#                                 Stratified by isBugCovering
-#                                   TRUE           FALSE         SMD 
-# n                                  22            26               
-# years_programming (mean (SD))   18.27 (13.18) 13.94 (11.04)  0.356
-# qualification_score (mean (SD))  4.09 (0.81)   4.00 (0.94)   0.104
-# volume_Halstead (mean (SD))     52.84 (30.27) 48.94 (6.83)   0.178
-# profession (%)                                               0.612
-# Professional_Developer           8 (36.4)     16 (61.5)        
-# Hobbyist                         8 (36.4)      4 (15.4)        
-# Graduate_Student                 0 ( 0.0)      0 ( 0.0)        
-# Undergraduate_Student            4 (18.2)      5 (19.2)        
-# Other                            2 ( 9.1)      1 ( 3.8) 
+# Stratified by is_fast
+#                                 FALSE         TRUE          SMD   
+# n                                 9            69               
+# years_programming (mean (SD))  2.22 (1.30)   4.88 (5.96)   0.617
+# age (mean (SD))               26.78 (5.14)  31.10 (8.97)   0.591
+# test_duration (mean (SD))      1.32 (0.88)   5.11 (3.07)   1.677
+# profession (%)                                             1.382
+# Graduate_Student               1 (11.1)      9 (13.0)        
+# Hobbyist                       2 (22.2)     25 (36.2)        
+# Other                          0 ( 0.0)      6 ( 8.7)        
+# Professional                   0 ( 0.0)     16 (23.2)        
+# Programmer                     1 (11.1)      0 ( 0.0)        
+# Undergraduate_Student          5 (55.6)     13 (18.8)        
 
 df_file_name <- df_selected[df_selected$file_name=="HIT03_6",]
 raw_table <- CreateTableOne(vars=xvars,strata="is_fast",data=df_file_name,test=FALSE)
 print(raw_table, smd=TRUE)
-#                                 Stratified by isBugCovering
-#                                   TRUE           FALSE         SMD 
-# n                                   45            155               
-# years_programming (mean (SD))    10.42 (8.53)    9.01 (8.02)   0.171
-# qualification_score (mean (SD))   4.18 (0.81)    4.15 (0.75)   0.038
-# volume_Halstead (mean (SD))     488.84 (563.39) 90.72 (97.40)  0.985
-# profession (%)                                                 0.359
-# Professional_Developer           28 (62.2)      77 (49.7)        
-# Hobbyist                          7 (15.6)      28 (18.1)        
-# Graduate_Student                  2 ( 4.4)      21 (13.5)        
-# Undergraduate_Student             6 (13.3)      22 (14.2)        
-# Other                             2 ( 4.4)       7 ( 4.5)  
+    > raw_table_all <- CreateTableOne(vars=xvars,strata="is_fast",data=df_selected,test=FALSE)
+  > print(raw_table_all, smd=TRUE)
+  # Stratified by is_fast
+  #                                FALSE         TRUE          SMD   
+  # n                               150          1638               
+  # years_programming (mean (SD))  2.82 (3.19)   5.72 (6.84)   0.544
+  # age (mean (SD))               27.61 (7.32)  30.46 (8.70)   0.354
+  # test_duration (mean (SD))      1.31 (0.70)   5.47 (3.26)   1.763
+  # profession (%)                                             0.898
+  # Graduate_Student              23 (15.3)    260 (15.9)        
+  # Hobbyist                      44 (29.3)    440 (26.9)        
+  # Other                         11 ( 7.3)    101 ( 6.2)        
+  # Professional                   0 ( 0.0)    417 (25.5)        
+  # Programmer                    13 ( 8.7)     36 ( 2.2)        
+  # Undergraduate_Student         59 (39.3)    384 (23.4)        
+  
 
-df_file_name <- df_selected[df_selected$file_name=="HIT04_7",]
-raw_table <- CreateTableOne(vars=xvars,strata="is_fast",data=df_file_name,test=FALSE)
-print(raw_table, smd=TRUE)
-#                                 Stratified by isBugCovering
-#                                   TRUE           FALSE         SMD 
-# n                                   73             441                
-# years_programming (mean (SD))     8.23 (10.54)    7.61 (8.72)    0.064
-# qualification_score (mean (SD))   4.21 (0.80)     4.14 (0.77)    0.085
-# volume_Halstead (mean (SD))     254.61 (300.61) 160.44 (173.94)  0.383
-# profession (%)                                                   0.134
-# Professional_Developer           22 (30.1)      147 (33.3)         
-# Hobbyist                         13 (17.8)       84 (19.0)         
-# Graduate_Student                 13 (17.8)       70 (15.9)         
-# Undergraduate_Student            17 (23.3)      106 (24.0)         
-# Other                             8 (11.0)       34 ( 7.7)  
+  df_file_name <- df_selected[df_selected$file_name=="HIT04_7",]
+  raw_table <- CreateTableOne(vars=xvars,strata="is_fast",data=df_file_name,test=FALSE)
+  print(raw_table, smd=TRUE)
+  # Stratified by is_fast
+  #                                 FALSE         TRUE          SMD   
+  # n                                57           649               
+  # years_programming (mean (SD))  2.73 (2.39)   5.18 (6.65)   0.491
+  # age (mean (SD))               27.16 (8.66)  29.97 (8.43)   0.329
+  # test_duration (mean (SD))      1.32 (0.65)   5.49 (3.42)   1.690
+  # profession (%)                                             0.859
+  # Graduate_Student              12 (21.1)    104 (16.0)        
+  # Hobbyist                      18 (31.6)    168 (25.9)        
+  # Other                          3 ( 5.3)     47 ( 7.2)        
+  # Professional                   0 ( 0.0)    156 (24.0)        
+  # Programmer                     4 ( 7.0)      8 ( 1.2)        
+  # Undergraduate_Student         20 (35.1)    166 (25.6) 
 
 df_file_name <- df_selected[df_selected$file_name=="HIT05_35",]
 raw_table <- CreateTableOne(vars=xvars,strata="is_fast",data=df_file_name,test=FALSE)
 print(raw_table, smd=TRUE)
-#                                 Stratified by isBugCovering
-#                                   TRUE           FALSE         SMD 
-# n                                   28            56               
-# years_programming (mean (SD))    10.29 (8.18)  10.34 (8.06)   0.007
-# qualification_score (mean (SD))   4.14 (0.89)   4.14 (0.88)  <0.001
-# volume_Halstead (mean (SD))     114.95 (34.26) 95.03 (40.20)  0.533
-# profession (%)                                                0.067
-# Professional_Developer           17 (60.7)     33 (58.9)        
-# Hobbyist                          3 (10.7)      6 (10.7)        
-# Graduate_Student                  2 ( 7.1)      5 ( 8.9)        
-# Undergraduate_Student             2 ( 7.1)      4 ( 7.1)        
-# Other                             4 (14.3)      8 (14.3) 
+# Stratified by is_fast
+#                                 FALSE         TRUE          SMD   
+# n                                 6            60               
+# years_programming (mean (SD))  2.50 (3.73)   8.22 (8.41)   0.879
+# age (mean (SD))               25.83 (2.04)  32.12 (9.76)   0.891
+# test_duration (mean (SD))      1.62 (1.10)   6.10 (3.66)   1.656
+# profession (%)                                             3.310
+# Graduate_Student               0 ( 0.0)     14 (23.3)        
+# Hobbyist                       0 ( 0.0)     17 (28.3)        
+# Other                          3 (50.0)      0 ( 0.0)        
+# Professional                   0 ( 0.0)     18 (30.0)        
+# Programmer                     1 (16.7)      3 ( 5.0)        
+# Undergraduate_Student          2 (33.3)      8 (13.3)  
 
 df_file_name <- df_selected[df_selected$file_name=="HIT06_51",]
 raw_table <- CreateTableOne(vars=xvars,strata="is_fast",data=df_file_name,test=FALSE)
 print(raw_table, smd=TRUE)
-#                                 Stratified by isBugCovering
-#                                   TRUE           FALSE         SMD 
-# n                                   31            145               
-# years_programming (mean (SD))    11.58 (10.39)   8.83 (8.35)   0.292
-# qualification_score (mean (SD))   4.52 (0.68)    4.18 (0.75)   0.471
-# volume_Halstead (mean (SD))     408.51 (387.85) 57.39 (68.17)  1.261
-# profession (%)                                                 0.292
-# Professional_Developer           13 (41.9)      59 (40.7)        
-# Hobbyist                          7 (22.6)      28 (19.3)        
-# Graduate_Student                  6 (19.4)      19 (13.1)        
-# Undergraduate_Student             3 ( 9.7)      24 (16.6)        
-# Other                             2 ( 6.5)      15 (10.3)  
+# Stratified by is_fast
+#                                 FALSE         TRUE          SMD   
+# n                                14           132               
+# years_programming (mean (SD))  3.07 (2.64)   6.06 (7.20)   0.552
+# age (mean (SD))               30.43 (7.70)  29.69 (7.87)   0.095
+# test_duration (mean (SD))      1.28 (0.79)   5.21 (2.81)   1.910
+# profession (%)                                             1.047
+# Graduate_Student               1 ( 7.1)     17 (12.9)        
+# Hobbyist                       5 (35.7)     39 (29.5)        
+# Other                          0 ( 0.0)      4 ( 3.0)        
+# Professional                   0 ( 0.0)     35 (26.5)        
+# Programmer                     2 (14.3)      5 ( 3.8)        
+# Undergraduate_Student          6 (42.9)     32 (24.2)    
+
+ 
 df_file_name <- df_selected[df_selected$file_name=="HIT07_33",]
 raw_table <- CreateTableOne(vars=xvars,strata="is_fast",data=df_file_name,test=FALSE)
 print(raw_table, smd=TRUE)
-#                                 Stratified by isBugCovering
-#                                   TRUE           FALSE         SMD 
-# n                                  21            48               
-# years_programming (mean (SD))    9.48 (8.91)  10.17 (7.00)   0.086
-# qualification_score (mean (SD))  4.52 (0.75)   4.27 (0.74)   0.340
-# volume_Halstead (mean (SD))     71.11 (45.85) 88.17 (54.17)  0.340
-# profession (%)                                               0.516
-# Professional_Developer           7 (33.3)     26 (54.2)        
-# Hobbyist                         9 (42.9)     14 (29.2)        
-# Graduate_Student                 1 ( 4.8)      2 ( 4.2)        
-# Undergraduate_Student            1 ( 4.8)      0 ( 0.0)        
-# Other                            3 (14.3)      6 (12.5)  
+# Stratified by is_fast
+#                                 FALSE         TRUE          SMD   
+# n                                 6            43               
+# years_programming (mean (SD))  6.67 (6.62)   6.41 (6.48)   0.040
+# age (mean (SD))               31.33 (7.12)  30.60 (6.13)   0.110
+# test_duration (mean (SD))      1.36 (0.43)   5.22 (3.77)   1.438
+# profession (%)                                             0.995
+# Graduate_Student               1 (16.7)      4 ( 9.3)        
+# Hobbyist                       3 (50.0)     15 (34.9)        
+# Other                          1 (16.7)      4 ( 9.3)        
+# Professional                   0 ( 0.0)     13 (30.2)        
+# Programmer                     0 ( 0.0)      1 ( 2.3)        
+# Undergraduate_Student          1 (16.7)      6 (14.0)   
 
 df_file_name <- df_selected[df_selected$file_name=="HIT08_54",]
 raw_table <- CreateTableOne(vars=xvars,strata="is_fast",data=df_file_name,test=FALSE)
 print(raw_table, smd=TRUE)
-#                                 Stratified by isBugCovering
-#                                   TRUE           FALSE         SMD 
-# n                                   31             210                
-# years_programming (mean (SD))    11.55 (13.08)    9.16 (8.54)    0.217
-# qualification_score (mean (SD))   4.13 (0.72)     4.03 (0.83)    0.129
-# volume_Halstead (mean (SD))     689.43 (746.43) 112.97 (146.47)  1.072
-# profession (%)                                                   0.389
-# Professional_Developer           14 (45.2)       77 (36.7)         
-# Hobbyist                          4 (12.9)       51 (24.3)         
-# Graduate_Student                  3 ( 9.7)       20 ( 9.5)         
-# Undergraduate_Student             4 (12.9)       38 (18.1)         
-# Other                             6 (19.4)       24 (11.4)  
-
+# Stratified by is_fast
+#                                 FALSE         TRUE          SMD   
+# n                                29           323               
+# years_programming (mean (SD))  1.86 (1.87)   5.59 (6.53)   0.776
+# age (mean (SD))               26.72 (7.19)  30.43 (8.75)   0.462
+# test_duration (mean (SD))      1.25 (0.53)   5.39 (2.90)   1.985
+# profession (%)                                             0.937
+# Graduate_Student               5 (17.2)     51 (15.8)        
+# Hobbyist                      10 (34.5)     79 (24.5)        
+# Other                          0 ( 0.0)     19 ( 5.9)        
+# Professional                   0 ( 0.0)     77 (23.8)        
+# Programmer                     2 ( 6.9)     10 ( 3.1)        
+# Undergraduate_Student         12 (41.4)     87 (26.9) 
