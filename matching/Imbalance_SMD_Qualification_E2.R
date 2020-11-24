6e29ff90e6237fcb8365a210d3f58c63a7fd9365
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-11-24 23:58:17 +0100

    Update Imbalance_SMD_Qualification_E2.R

--- matching/Imbalance_SMD_Qualification_E2.R
+++ matching/Imbalance_SMD_Qualification_E2.R
@@ -1,9 +1,12 @@
 "Check imbalances in Standardized Mean Differences - SMD
 
 
-Treatment: Qualification Score (IRT Score)
-Analyze imbalance for each Task (HIT01 to HIT08)
+Treatment: is_fast 
+Outcome: adjusted_score (Qualification IRT Score)
+
+(DONE) Analyze imbalance for each Task (HIT01 to HIT08)
 Analyze imbalance across professions
+
 plotting results with Cobalt
 https://cran.rstudio.com/web/packages/cobalt/vignettes/cobalt_A4_love.plot.html
 
