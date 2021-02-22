a632d8a7e4aa0b816ae81cdd8fb3a52bdd9333d2
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-02-22 01:53:06 +0100

    Update merge_task_consent_E2.R
    Finished integrating the scripts. Still need to test if the data is consistent.

--- data_loaders/merge_task_consent_E2.R
+++ data_loaders/merge_task_consent_E2.R
@@ -7,5 +7,38 @@ load_consent_create_indexes_E2.R
 load_ground_truth_E2.R
 create_indexes_E2.R
 
+- Loads merged data of Tasks and Consent (demographics) from file merged_tasks_complexity_E2.csv
+- Loads the ground truth for experiment E2
+
+the consent data already includes 
+- Includes IRT (item response theory) equalization of the grades
+
+- Includes Add Halstead complexity of each program statement
+
+TODO:
+- Test if the data is correct. The last step shrunk from 85 to 66 columns. Strange.
+
+
 "
 
+#----------------
+#Load tasks with ground truth and complexity
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_ground_truth_E2.R")
+df_E2_ground <- load_ground_truth();
+
+#----------------
+#Load consent data
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E2.R")
+df_consent <- load_consent_create_indexes();
+
+#----------------
+#Merge worker and task data
+
+"Left-join worker_id, worker_id"
+df_E2_final <- left_join(x=df_E2_ground,y=df_consent,keep=TRUE, by=c("worker_id"="worker_id","file_name"="file_name"))
+dim(df_E2_final) 
+
+#----------------
+#Create indexes
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//create_indexes_E2.R")
+df_E2 <- create_indexes(df_E2_ground)
