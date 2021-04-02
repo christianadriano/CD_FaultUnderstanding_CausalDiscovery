64447c5dfc637e3e54c7c9c00ed06fdb93be0c4b
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-04-03 00:04:57 +0200

    Update validate_Consent_IRT_E2.R

--- data_loaders/validate_Consent_IRT_E2.R
+++ data_loaders/validate_Consent_IRT_E2.R
@@ -2,7 +2,7 @@
 Validation of the consent file E2 with IRT data
 
 1- Check if same workerID has different demographics
-2- Check if same workerID has same adjusted qualification score and profession
+2- Check if same workerID has same adjusted qualification score
 
 "
 
@@ -11,6 +11,7 @@ Validation of the consent file E2 with IRT data
 source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E2.R")
 df_consent <- load_consent_create_indexes();
 
+#-------------------------------------------------------------
 #1-check if there are workerID with different demographics
 worker_id_list <- unique(df_consent$worker_id)
 
@@ -32,6 +33,7 @@ for (worker_id in worker_id_list) {
 # [1] "45ea8E6e0-10:95cg-5I0I151_3"
 
 #-------------------------------------------------------
+#2- Check if same workerID has same adjusted qualification score
 
 
 for (worker_id in worker_id_list) {
@@ -45,10 +47,10 @@ for (worker_id in worker_id_list) {
 
 # [1] "358ig-7C7C5-1-2:10EI0c-3A0-3-9_3"
 # [1] "27Ei-8i0A-3-59:15cG-7i7C996_3"
-# [1] "23EE-1c8E1-7-2:16eA-3g-9A-7-5-3_3"
+# [1] "23EE-1c8E1-7-2:16eA-3g-9A-7-5-3_3"  <<<<<
 # [1] "832cg-7G1i-462:73eI-8E-2g-985_3"
 # [1] "270ie-2C-4c-550:97ai0I-8C871_3"
-# [1] "519CE-1a-8G700:117Ec-7c4a-50-9:93Ce-7c-8i-69-4_3"
+# [1] "519CE-1a-8G700:117Ec-7c4a-50-9:93Ce-7c-8i-69-4_3" <<<<<
 # [1] "67AI-8G5A71-3:163Aa0i0a963:161eg5c5I-649_3"
 # [1] "94iC8I-7g2-6-9:176ac-2C-6e7-94_3"
 # [1] "815aA-9g-4e44-8:182Ae3g-6i-30-5_3"
@@ -73,12 +75,12 @@ for (worker_id in worker_id_list) {
 # [1] "892CA6a-8c802:759GA-8a-5a-285_3"
 # [1] "8EI3i9I949:931ii-1c6G-5-95_3"
 # [1] "1582ai0e2I065_3"
-# [1] "18ii-3A8a-2-2-3:17cC4a2e-8-2-6_3"
+# [1] "18ii-3A8a-2-2-3:17cC4a2e-8-2-6_3"<<<<<
 # [1] "56II-1e7E-789:57CI-4g3G20-9_3"
 # [1] "80ca6g0i-1-49:23iE-4i0a1-40_3"
 # [1] "59eA3i6i030:86GA-3G-4C8-5-2_3"
 # [1] "92ea8E5i07-8:77cc-5c0G32-4_3"
-# [1] "75IG0G-7C6-30:286AC-3A-2g0-74:3aI0C8I-9-2-5_3"
+# [1] "75IG0G-7C6-30:286AC-3A-2g0-74:3aI0C8I-9-2-5_3"<<<<< 
 # [1] "63GI-8C5a906:400EE-2a-6c-4-89_3"
 # [1] "98cg8a0A69-1:439ee9e-2i-106_3"
 # [1] "28Gc-2i-9C-18-2:442cA-6G6i-749_3"
@@ -88,13 +90,13 @@ for (worker_id in worker_id_list) {
 # [1] "680ce-8i-5C06-6:70Ec7A4e-7-84_3"
 # [1] "114eE1e0a9-5-8:820Ie-3c1C-85-5_3"
 # [1] "103Ee-8a6i-3-7-8:873AG0a3i770_3"
-# [1] "922iC0E-2E713:117cA2E0I8-80_3"
+# [1] "922iC0E-2E713:117cA2E0I8-80_3"<<<<<<<<<<<<<<<<<<<<<<<
 # [1] "101GC1i5g-7-61:1008II-5a4e60-9:110gg-3i2G-6-94_3"
 # [1] "1198eA8G-7e7-7-3:120Gc-7G5G4-5-4_3"
 # [1] "66AC-5a0g-47-9:1443IA-7C-6e967_3"
 # [1] "1600Cc-8c9E841:67GE4A0g-870_3"
 # [1] "83gE-9E9a-176:1609ca8I0E-894_3"
-# [1] "45ea8E6e0-10:95cg-5I0I151_3"
+# [1] "45ea8E6e0-10:95cg-5I0I151_3"<<<<<<<<<<<<<<<<<<<<<<<<
 # [1] "4IC-5i7I612:81ia8g6g-3-69:10IE-1i1e662_3"
 
 
