cddfb7f6ebc4b648d94aa6b601724d43298c2005
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-04-03 00:05:59 +0200

    Update validate_Consent_IRT_E2.R

--- data_loaders/validate_Consent_IRT_E2.R
+++ data_loaders/validate_Consent_IRT_E2.R
@@ -25,12 +25,12 @@ for (worker_id in worker_id_list) {
 
 # Follows the problematic worker_ids
 # [1] "23EE-1c8E1-7-2:16eA-3g-9A-7-5-3_3"
-# [1] "519CE-1a-8G700:117Ec-7c4a-50-9:93Ce-7c-8i-69-4_3"
-# [1] "1506IC2A8e2-20:615gC7g-8i-21-3:85aA-1G-5e8-11:89cG-4a4I-5-15_3"
-# [1] "18ii-3A8a-2-2-3:17cC4a2e-8-2-6_3"
-# [1] "75IG0G-7C6-30:286AC-3A-2g0-74:3aI0C8I-9-2-5_3"
-# [1] "922iC0E-2E713:117cA2E0I8-80_3"
-# [1] "45ea8E6e0-10:95cg-5I0I151_3"
+# [2] "519CE-1a-8G700:117Ec-7c4a-50-9:93Ce-7c-8i-69-4_3"
+# [3] "1506IC2A8e2-20:615gC7g-8i-21-3:85aA-1G-5e8-11:89cG-4a4I-5-15_3"
+# [4] "18ii-3A8a-2-2-3:17cC4a2e-8-2-6_3"
+# [5] "75IG0G-7C6-30:286AC-3A-2g0-74:3aI0C8I-9-2-5_3"
+# [6] "922iC0E-2E713:117cA2E0I8-80_3"
+# [7] "45ea8E6e0-10:95cg-5I0I151_3"
 
 #-------------------------------------------------------
 #2- Check if same workerID has same adjusted qualification score
