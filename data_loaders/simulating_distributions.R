4fad46e100f743648ea7ec288785ecf086f6301f
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-04-07 23:17:29 +0200

    TODO list updated
    Apply simulation to compute uncertainty intervals in different ways.

--- data_loaders/simulating_distributions.R
+++ data_loaders/simulating_distributions.R
@@ -1,6 +1,13 @@
 "
 Simulating distributions
 
+TODO: 
+- Load my data and fit a regression using glmstan 
+- Make an example with replicate
+- Check if coefficients are within 1 and 2 standard deviations
+- Compute uncertainty interval assuming a t distribution
+- Compute uncertainty interval from the fit model
+- Reproduce these uncertainty computations for the EM models fast and slow answers E2
 "
 a <- rep(NA,10)
 for (i in c(1:10)) {
