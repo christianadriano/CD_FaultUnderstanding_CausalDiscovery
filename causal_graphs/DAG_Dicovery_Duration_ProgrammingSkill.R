975d09d0cff49db7116d2b5bfaf93a7f3cfa3831
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-07-18 23:58:59 +0200

    renamed years_programming

--- causal_graphs/DAG_Dicovery_Duration_ProgrammingSkill.R
+++ causal_graphs/DAG_Dicovery_Duration_ProgrammingSkill.R
@@ -21,35 +21,37 @@ source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding/
 df_consent_fast <- df_consent[df_consent$is_fast,]
 df_consent_slow <- df_consent[!df_consent$is_fast,]
 
+df_consent <- rename(df_consent,speed=testDuration_fastMembership)
+df_consent <- rename(df_consent,years_prog=years_programming)
+
 df_selected <-
-  dplyr::select(df_consent_fast,
+  dplyr::select(df_consent,
                 profession, #categorical
                 age,
-                years_programming,
+                years_prog,
                 test_duration,
-                #is_fast, #binary
+                speed, #or use is_fast, which is binary
                 adjusted_score #outcome
                 );
 
 df_selected$profession <- as.factor(df_selected$profession)
-#df_selected$is_fast <- as.factor(df_selected$is_fast)
 
 node.names <- colnames(df_selected)
 
-#years_programming is not parent of age.
-blacklist_1 <- data.frame(from = c("years_programming"), 
+#years_prog is not parent of age.
+blacklist_1 <- data.frame(from = c("years_prog"), 
                           to   = c("age"))
 
-#Prevent is_fast to be parents of age and years_programming and profession
-#blacklist_2 <- data.frame(from = c("is_fast"), 
-#                            to   = c("years_programming","age","profession"))
+#Prevent speed to be parents of age and years_prog and profession
+blacklist_2 <- data.frame(from = c("speed"), 
+                          to   = c("years_prog","age","profession"))
 
 #profession has parent nodes
 blacklist_3 <- data.frame(from = node.names[-grep("profession", node.names)], 
                           to   = c("profession"))
-#test_duration is not parent of age, years_programming, profession
+#test_duration is not parent of age, years_prog, profession
 blacklist_4 <- data.frame(from = c("test_duration"),
-                          to   = c("profession","years_programming","age"))#,"is_fast"))
+                          to   = c("profession","years_prog","age"))#,"is_fast"))
 #adjusted_score cannot be parent of anyone
 blacklist_5 <- data.frame(from = c("adjusted_score"),
                           to   = node.names[-grep("adjusted_score", node.names)])
@@ -74,8 +76,8 @@ plot(bn,main="All Professions, Score-Based Discovery")
 #Remove profession from blacklist
 blacklist_all <- blacklist_all[!(blacklist_all$from %in% c("profession") ),]
 blacklist_all <- blacklist_all[!(blacklist_all$to %in% c("profession") ),]
-blacklist_all <- blacklist_all[!(blacklist_all$from %in% c("is_fast") ),]
-blacklist_all <- blacklist_all[!(blacklist_all$to %in% c("is_fast") ),]
+#blacklist_all <- blacklist_all[!(blacklist_all$from %in% c("speed") ),]
+#blacklist_all <- blacklist_all[!(blacklist_all$to %in% c("speed") ),]
 
 
 #Run structure discovery for each profession
@@ -88,8 +90,9 @@ for (i in 1:length(professions)) {
   df_prof <- df_selected[df_selected$profession==choice,]
   df_prof <- 
     dplyr::select(df_prof,
-                  years_programming,
+                  years_prog,
                   age,
+                  speed,
                   test_duration,
                   adjusted_score
     );
@@ -100,7 +103,7 @@ for (i in 1:length(professions)) {
 
 "Analysis of results of the PC algorithm
 test duration seem relevant only for professional, undergrad, grad, hobbyist
-only in undegrad that test duration is affected by years_programming"
+only in undegrad that test duration is affected by years_prog"
 
 #Score-based algorithm - Hill Climbing
 for (i in 1:length(professions)) {
@@ -108,8 +111,9 @@ for (i in 1:length(professions)) {
   df_prof <- df_selected[df_selected$profession==choice,]
   df_prof <- 
     dplyr::select(df_prof,
-                  years_programming,
+                  years_prog,
                   age,
+                  speed,
                   test_duration,
                   adjusted_score
     );
@@ -122,44 +126,53 @@ for (i in 1:length(professions)) {
 Test duration has not effect on adjusted_score for other and Programmer
 Test duration has no parents for Graduate and Professional
 Only in Hobbyists that test duration is a mediator for effect on adjusted_score
-Test duration has years_programming as parent in Hobbyist, Undergrad, 
+Test duration has years_prog as parent in Hobbyist, Undergrad, 
 Programmer, and Other.
 "
 
 #-------------------------------------
-#USing now the qualification_score
+#Using now the qualification_score
 
 df_selected <-
-  dplyr::select(df_E2_ground,
-                years_programming,
-                test_duration,
-                qualification_score,
+  dplyr::select(df_consent,
+                profession, #categorical
                 age,
-                profession
+                years_prog,
+                test_duration,
+                speed, #or use is_fast, which is binary
+                qualification_score #outcome
   );
+
 
 node.names <- colnames(df_selected)
 
-#years_programming is not parent of age.
-blacklist_1 <- data.frame(from = c("years_programming"), 
+#years_prog is not parent of age.
+blacklist_1 <- data.frame(from = c("years_prog"), 
                           to   = c("age"))
 #profession has parent nodes
 blacklist_2 <- data.frame(from = node.names[-grep("profession", node.names)], 
                           to   = c("profession"))
-#test_duration is not parent of age, years_programming, profession
+#test_duration is not parent of age, years_prog, profession
 blacklist_3 <- data.frame(from = c("test_duration"),
-                          to   = c("profession","years_programming","age"))
-#adjusted_score cannot be parent of anyone
+                          to   = c("profession","years_prog","age"))
+#qualification_score cannot be parent of anyone
 blacklist_4 <- data.frame(from = c("qualification_score"),
                           to   = node.names[-grep("qualification_score", node.names)])
+#Prevent speed to be parents of age and years_prog and profession
+blacklist_5 <- data.frame(from = c("speed"), 
+                          to   = c("years_prog","age","profession"))
 
-blacklist_all <- rbind(blacklist_1,blacklist_2,blacklist_3,blacklist_4) 
+
+blacklist_all <- rbind(blacklist_1,blacklist_2,blacklist_3,blacklist_4,blacklist_5) 
 
 #------------------------------------------
 #Including Profession
 
+bn <- pc.stable(df_selected,blacklist = blacklist_all)
+plot(bn,main="All Professions, Constraint-Based Discovery")
+
 bn <-tabu(df_selected,blacklist = blacklist_all)
-plot(bn,main="All Professions")
+plot(bn,main="All Professions, Score-Based Discovery")
 
 
 #Remove profession from blacklist
@@ -172,10 +185,11 @@ for (i in 1:length(professions)) {
   df_prof <- df_selected[df_selected$profession==choice,]
   df_prof <- 
     dplyr::select(df_prof,
-                  years_programming,
+                  years_prog,
                   age,
+                  speed,
                   test_duration,
-                  qualification_score
+                  adjusted_score
     );
   bn <-pc.stable(df_prof,blacklist = blacklist_all)
   plot(bn,main=choice)
@@ -184,7 +198,7 @@ for (i in 1:length(professions)) {
 
 "Analysis of results of the PC algorithm
 Test duration has not effect on adjusted_score for other and Programmer
-Only in undegrad that test duration is affected by years_programming
+Only in undegrad that test duration is affected by years_prog
 Hence, qualification_Score and adjusted_score produced the same graphs with the PC algorithm
 "
 
@@ -194,10 +208,11 @@ for (i in 1:length(professions)) {
   df_prof <- df_selected[df_selected$profession==choice,]
   df_prof <- 
     dplyr::select(df_prof,
-                  years_programming,
+                  years_prog,
                   age,
+                  speed,
                   test_duration,
-                  qualification_score
+                  adjusted_score
     );
   bn <-tabu(df_prof,blacklist = blacklist_all)
   plot(bn,main=choice)
@@ -208,7 +223,7 @@ for (i in 1:length(professions)) {
 Test duration has not effect on adjusted_score for other and Programmer
 Test duration has no parents for Graduate and Professional
 Only in Hobbyists that test duration is a mediator for effect on adjusted_score
-Test duration has years_programming as parent in Hobbyist, Undergrad, 
+Test duration has years_prog as parent in Hobbyist, Undergrad, 
 Programmer, and Other.
 
 Hence, qualification_score and adjusted_score produced the same graph with the tabu algorithm
