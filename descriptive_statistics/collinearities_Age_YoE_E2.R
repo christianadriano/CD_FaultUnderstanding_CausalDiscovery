5d87ed9f5a97ff75f34b951a29d630ab76e51e55
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-09-23 23:43:47 +0200

    Update collinearities_Age_YoE_E2.R

--- descriptive_statistics/collinearities_Age_YoE_E2.R
+++ descriptive_statistics/collinearities_Age_YoE_E2.R
@@ -5,17 +5,16 @@ source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding/
 
 df_consent <- load_consent_create_indexes()
 #Remove NA's
-df_E2 <- df_consent[complete.cases(df_consent[,c("years_programming","qualification_score")]),]
+df_E2 <- df_consent[complete.cases(df_consent[,c("years_programming","age")]),]
 
 # standardize variables = (zero centered, standard deviation one)
 df_E2$yoe <- scale(df_E2$years_programming)
-df_E2$score <- scale(df_E2$qualification_score)
 df_E2$age <- scale(df_E2$age)
 
 #------------------
 #ALL
 
-r <- cor.test(x=df_E2$yoe, y=df_E2$age, method="kendall")
+cor.test(x=df_E2$yoe, y=df_E2$age, method="kendall")
 #z = 15.679, p-value < 2.2e-16, tau=0.2647107 
 #Medium correlated.
 
