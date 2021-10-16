ca61525a3de07d1a4f1b87ed7830bf12f2551c17
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-10-16 23:54:26 +0200

    Update MixedModels_Causal_Age-Yoe-Duration_E2.R

--- causal_models/programming_score/MixedModels_Causal_Age-Yoe-Duration_E2.R
+++ causal_models/programming_score/MixedModels_Causal_Age-Yoe-Duration_E2.R
@@ -16,41 +16,28 @@ library(brms)
 library(stringr)
 library(dplyr)
 
-#Load data
-source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//load_consent_create_indexes_E2.R")
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//remove_outliers_E2.R")
 
-dim(df_E2) #3657   32
+#Load data
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E2.R")
+df_consent <- load_consent_create_indexes()
+dim(df_consent) #1788   29
 
 #Remove participants for which we do not have years_programming
-df <- df_E2 %>% drop_na(years_programming) #initial 3567, left with 2062 rows
+df <- df_consent %>% drop_na(years_programming)
 
 df$yoe <- df$years_programming #scale(df$years_programming)
 df$ages <- df$age #scale(df$age)
 
-dim(df) #2062 32 Reduced 1595 rows
+dim(df) #1788   31
 
 #-----------------
-"OUTLIERS in DURATION"
-"Code comprehension studies show that a programmer takes from 12 to 24 seconds is also the 
-average minimum time to read one line of code."
 
-"The lower cut to the minimum time to read all 5 questions and corresponding lines of code
-in the qualification test. 
- Since the test has 5 questions, each question 
-requires the inspection of one line of code, that would require the programmer from 60s to 120s.
-We chose 120s (2 min) as the minimum time-effort one need to read and answer all 5 questions"
+df <- remove_outliers_test_duration(df)
+dim(df) #1691   29
 
-#The upper cut corresponds to 10 times the minimum. We choose 24s, so 60 min.
-
-df <- df[df$testDuration_minutes<=15 & df$testDuration_minutes>=1 ,]
-dim(df_E2) #3657   30
-
-#Remove participants for whom we do not have test duration
-df <- df %>% drop_na(testDuration_minutes)
-dim(df) #1229   32
-
-boxplot(df$testDuration_minutes)
-summary(df$testDuration_minutes)
+boxplot(df$test_duration)
+summary(df$test_duration)
 
 #-------------------------
 "AGE > YoE
