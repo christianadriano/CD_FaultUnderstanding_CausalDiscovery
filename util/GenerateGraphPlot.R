58b31c16a1288a5a9c6c56034617bafef6482f3d
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2022-02-14 00:41:42 +0100

    Update GenerateGraphPlot.R

--- util/GenerateGraphPlot.R
+++ util/GenerateGraphPlot.R
@@ -9,19 +9,19 @@ save_bayesian_net_plot <- function(bayesian_net,outcome_node,plot_title,
   gR <- graphviz.plot(bn,render = FALSE,
                       main=plot_title,
                       shape=c("ellipse"));
-  gR = layoutGraph(gR, attrs = list(graph = list(rankdir = "LR")))
+  gR = layoutGraph(gR, attrs = list(graph = list(rankdir = "LR",nodesep=0.1)))
   
   graph.par(list(nodes=list(col="black", lty="solid", 
-                            lwd=1, fontsize=14),
-                 graph=list(cex.main=0.8)
+                            lwd=1, fontsize=10),
+                 graph=list(cex.main=2.8)
   ))
   
   nodeRenderInfo(gR)$fill[c(outcome_node)]="lightblue"
   
-  png(filename=paste0(folder,file_name,".png"))
-  #,
-  #    height=200,width=600,antialias = "cleartype",
-  #    bg = "transparent");
+  png(filename=paste0(folder,file_name,".png"),
+      height=200,width=600,antialias = "cleartype",
+      res = 100,unit="mm",
+      bg = "transparent");
   renderGraph(gR);
   dev.off();
 }
