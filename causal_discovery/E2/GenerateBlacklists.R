57c0f05ff4459a72ad30c8041ae8baec5243b9bd
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2022-02-17 18:04:36 +0100

    Update GenerateBlacklists.R

--- causal_discovery/E2/GenerateBlacklists.R
+++ causal_discovery/E2/GenerateBlacklists.R
@@ -18,3 +18,20 @@ generate_blacklist_E2_TestScore <- function(node.names, outcome.node){
   
   return(blacklist_all)
 }
+
+generate_blacklist_E1_TestScore <- function(node.names, outcome.node){
+  
+  #progr_years is not parent of partic_age
+  blacklist_1 <- data.frame(from = c("progr_years"), 
+                            to   = c("partic_age"))
+  #test_duration is not parent of partic_age, progr_years
+  blacklist_2 <- data.frame(from = c("test_duration"),
+                            to   = c("progr_years","partic_age")) 
+  #outcome.node cannot be parent of anyone
+  blacklist_3 <- data.frame(from = c(outcome.node),
+                            to   = node.names[-grep(outcome.node, node.names)])
+  
+  blacklist_all <- rbind(blacklist_1,blacklist_2,blacklist_3) 
+  
+  return(blacklist_all)
+}
