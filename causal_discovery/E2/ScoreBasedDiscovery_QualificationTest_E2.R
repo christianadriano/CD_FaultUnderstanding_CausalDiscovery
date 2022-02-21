fbe8c16f99652f71e5981bccbb0c6cf8484f0b74
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2022-02-21 23:51:16 +0100

    Update ScoreBasedDiscovery_QualificationTest_E2.R

--- causal_discovery/E2/ScoreBasedDiscovery_QualificationTest_E2.R
+++ causal_discovery/E2/ScoreBasedDiscovery_QualificationTest_E2.R
@@ -26,17 +26,6 @@ The library used was bnlearn \cite{scutari2010learning}
 https://www.bnlearn.com/documentation/man/structure.learning.html
 Learning Bayesian Networks with the bnlearn R - https://arxiv.org/pdf/0908.3817.pdf
 
-#TODO
-
-#Draw hypothetical graph (prior)
-
-#Profession not supported, so need to run the analysis by profession
-#Include speed_fast/slow
-#Specify the query on duration and YoE (the reflect on causal discovery)
-
-#compare graphs produced by each of the methods. Check how sensitive they are to false discovery rate.
-#compare how professions are distinct in terms of adjusted_score and qualification_score
-
 " 
 
 library(bnlearn)
@@ -50,12 +39,17 @@ source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding/
 source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//causal_discovery//GenerateBlacklists.R")
 
 
-#Load only Consent data. No data from tasks, only from demographics and qualification test
-source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E2.R")
-df_consent <- load_consent_create_indexes()
-df_consent <- rename(df_consent,progr_years=years_programming)
-df_consent <- rename(df_consent,test_score=adjusted_score)
-df_consent <- rename(df_consent,partic_age=age)
+prepareData <- function(){
+  #Load only Consent data. No data from tasks, only from demographics and qualification test
+  source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E2.R")
+  df_consent <- load_consent_create_indexes()
+  df_consent <- rename(df_consent,progr_years=years_programming)
+  df_consent <- rename(df_consent,test_score=adjusted_score)
+  df_consent <- rename(df_consent,partic_age=age)
+  return(df_consent)
+}
+
+df_consent <- prepareData()
 
 df_selected <-
   dplyr::select(df_consent,
@@ -65,20 +59,9 @@ df_selected <-
                 test_score #outcome variable
   );
 
-node.names <- colnames(df_selected)
-outcomeNode <- "test_score"
-
-#progr_years is not parent of partic_age
-blacklist_1 <- data.frame(from = c("progr_years"), 
-                          to   = c("partic_age"))
-#test_duration is not parent of partic_age, progr_years
-blacklist_2 <- data.frame(from = c("test_duration"),
-                          to   = c("progr_years","partic_age")) 
-#test_score cannot be parent of anyone
-blacklist_3 <- data.frame(from = c("test_score"),
-                          to   = node.names[-grep("test_score", node.names)])
-
-blacklist_all <- rbind(blacklist_1,blacklist_2,blacklist_3) 
+outcomeNode ="test_score"
+blacklist_all <- blacklist_E2_TestScore(node.names=colnames(df_selected), 
+                                        outcome.node=outcomeNode)
 
 #------------------------------------------
 #HC
@@ -100,7 +83,7 @@ save_bayesian_net_plot(bayesian_net=bn,
 "
 Both algorithms produced the same graph. It has two additional 
 edges than the graph discovered by the CB algorithms. 
-The two additional edges are years_prog->test_duration, age->adjusted_score
+The two additional edges are years_prog->test_duration, age->test_score
 "
 
 #----------------------------------------------
@@ -113,16 +96,14 @@ df_selected <-
                 partic_age,
                 progr_years,
                 test_duration,
-                test_score #outcome adjusted score
+                test_score #outcome
   );
-
-df_selected$profession <- as.factor(df_selected$profession)
 
 #Run structure discovery for each profession
 professions = c("Other", "Undergraduate_Student","Graduate_Student","Hobbyist",
                 "Programmer","Professional")
 
-#PC-STABLE and IAMB.FDR
+#Hill-Climbing and Tabu
 for (i in 1:length(professions)) {
   choice = professions[i]
   df_prof <- df_selected[df_selected$profession==choice,]
@@ -173,26 +154,14 @@ df_consent <- rename(df_consent,orig_score=qualification_score)
 df_selected <-
   dplyr::select(df_consent,
                 years_prog,
-                age,
+                partic_age,
                 test_duration,
                 orig_score #outcome
   );
 
 
-node.names <- colnames(df_selected)
-
-#years_prog is not parent of age.
-blacklist_1 <- data.frame(from = c("years_prog"), 
-                          to   = c("age"))
-#test_duration is not parent of age, years_prog
-blacklist_2 <- data.frame(from = c("test_duration"),
-                          to   = c("years_prog","age"))
-#orig_score cannot be parent of anyone
-blacklist_3 <- data.frame(from = c("orig_score"),
-                          to   = node.names[-grep("orig_score", node.names)])
-
-blacklist_all <- rbind(blacklist_1,blacklist_2,blacklist_3) 
-
+blacklist_all <- blacklist_E2_TestScore(node.names=colnames(df_selected), 
+                                        outcome.node="orig_score")
 
 #------------------------------------------
 #Including Profession as Node
@@ -207,13 +176,13 @@ bn <-iamb.fdr(df_selected,blacklist = blacklist_all)
 plot(bn,main="All Professions, iamb.fdr algorithm")
 
 #-----------------------------------------
-#Remove Profession as Node
+# Analysis by Profession
 
 df_selected <-
   dplyr::select(df_consent,
                 profession,
                 years_prog,
-                age,
+                partic_age,
                 test_duration,
                 orig_score #outcome
   );
@@ -224,7 +193,7 @@ for (i in 1:length(professions)) {
   df_prof <- 
     dplyr::select(df_prof,
                   years_prog,
-                  age,
+                  partic_age,
                   test_duration,
                   orig_score
     );
@@ -250,48 +219,34 @@ for (i in 1:length(professions)) {
 
 #------------------------------------------------------
 #SPEED CLUSTERS
+#Evaluate how fast and slow can explain test_score
 #------------------------------------------------------
-#Will only used TABU
-
-df_consent <- load_consent_create_indexes()
-df_consent <- rename(df_consent,progr_years=years_programming)
-
-df_selected <-
-  dplyr::select(df_consent,
-                profession,
-                progr_years,
-                age,
-                test_duration,
-                orig_score #outcome
-  );
-
-node.names <- colnames(df_selected)
-
-#years_prog is not parent of age.
-blacklist_1 <- data.frame(from = c("years_prog"), 
-                          to   = c("age"))
-#test_duration is not parent of age, years_prog,
-blacklist_2 <- data.frame(from = c("test_duration"),
-                          to   = c("years_prog","age")) 
-#adjusted_score cannot be parent of anyone
-blacklist_3 <- data.frame(from = c("adjusted_score"),
-                          to   = node.names[-grep("adjusted_score", node.names)])
-
-blacklist_all <- rbind(blacklist_1,blacklist_2,blacklist_3) 
-
-#------------------------------------------
-
-#Evaluate how fast and slow can explain adjusted_score score
-df_consent_fast <- df_consent[df_consent$is_fast,]
-df_consent_fast <-
-  dplyr::select(df_consent_fast,
-                years_prog,
-                age,
-                test_duration,
-                adjusted_score #outcome
-  );
-bn <- h2pc(df_consent_fast,blacklist = blacklist_all)
-plot(bn,main="FAST Answers (Tabu algorithm)")
+i=1
+for (i in 1:length(professions)) {
+  choice = professions[i]
+  df_prof <- df_consent[df_consent$profession==choice,]
+  median_membership <- df_prof$testDuration_fastMembership;
+  df_prof_fast <- df_prof[df_prof$testDuration_fastMembership>=median_membership,]
+  df_selected <- 
+    dplyr::select(df_prof_fast,
+                  progr_years,
+                  partic_age,
+                  test_duration,
+                  test_score
+    );
+  
+  blacklist_all <- blacklist_E2_TestScore(node.names=colnames(df_selected), 
+                                          outcome.node="test_score")
+  bn <- tabu(df_selected,blacklist = blacklist_all)
+  bn_name=paste("E2 Fast Speed (>Median)",choice," Original_Test_Score (tabu)");
+  save_bayesian_net_plot(bayesian_net=bn,
+                         outcome_node="test_score",
+                         plot_title=bn_name,
+                         file_name=bn_name,
+                         folder=plots_folder)
+  
+}
+  #plot(bn,main="FAST Answers (Tabu algorithm)")
 "FAST answers graph is identical to no clustering by answer speed.
 Other: yoe -> score, age -> yoe
 Undergrad: yoe -> score, age -> yoe, age->score, yoe->duration, duration->score
